aa75d0146321e8c462e9dfb5ff6a49102a05a8d3
Dimitri Grassi <contact@wordpresschef.it>
2015-02-10 14:35:04 +0000

    tab_documentation.php edited online with Bitbucket

--- views/settings/tab_documentation.php
+++ views/settings/tab_documentation.php
@@ -7,37 +7,53 @@
 <h3><em>Step 2</em></h3>
 <p><strong>Go to “Salon Settings” &#8211; &gt; “General” tab and provide the following information:</strong></p>
 <p>a) Your salon’s name<br />
-b) Your salon’s contact email address<br />
-c) Your salon’s reservations telephone number<br />
-d) Your salon’s address<br />
-e) Your salon’s “booking notes” &#8211; provide your customers crucial informations about your booking policy. This informations will be displayed at the end of booking process and inside the email booking notification.<br />
+      <br>
+      b) Your salon’s contact email address<br />
+      <br>
+      c) Your salon’s reservations telephone number<br />
+      <br>
+      d) Your salon’s address<br />
+      <br>
+      e) Your salon’s “booking notes” &#8211; provide your customers useful information about your booking policy. <br>
+      This information will be displayed at the end of booking process and inside the email booking notification.<br />
+      <br>
 f) set your salon’s social networks pages</p>
 <p><strong>When finish click “Update Settings” button</strong></p>
 <p>&nbsp;</p>
 <h3><em>Step 3</em></h3>
 <p><strong>Go to “Salon Settings” &#8211; &gt; “Booking Rules” tab and provide the following information:</strong></p>
-<p>a) How many people you can attend during a single time-session?</p>
-<p>You need to specify the number of people you can attend at the same time.</p>
-<p>For example: if in your salon you are alone, this means that you can attend only one person during a single “time-session”. Meanwhile if in your salon you work with another person, this means that you can attend 2 persons during a single time-session.. and so on.</p>
+<p>a) <strong>How many people you can attend during a single time-session?</strong></p>
+<p>Set the number of people you can attend at the same time.</p>
+<p>For example: if in your salon you are alone, this means that you can attend only one person during a single “time-session”. Meanwhile if in your salon you work with another person, this means that you can attend 2 persons during a single time-session... and so on.</p>
 <p>This setting is very crucial because it affects the number of online bookings that the plugin can accept for the same date/time.</p>
-<p>b) You need to specify how many hours or days in advance users can book for an appointment. You need to set a minimum and a maximum time-range.</p>
-<p>c) Set the duration of a single time-session.</p>
-<p>The duration of a single time-session is the average length of your services. For example: if you are a barber your should decide how long on average lasts a single hair cut or a shave.<br />
+<p>b) <strong>Define your time-session average duration <br>
+</strong><br>
+      The duration of a single time-session is the  length on average of your services. <br>
+      For example: if you are a barber your should decide how long on average lasts a single hair cut or a shave.<br />
+      <br>
 This setting affects the number of bookings allowed for the same  date/time.</p>
-<p>d) You need to set your salon’s timetable specifying the days and the time range when the bookings are allowed.</p>
-<p>Our plugin allows you to set multiple booking rules using “Add new” button.</p>
-<p>e) “Disable online booking” is an option that you can use when you need to turn-off the online booking system. You can even set a custom message for your users.</p>
-<p>f) “booking confirmation” option allows you to decide on single booking approval. On default settings this option is disabled. Every single booking is automatically approved by the system.</p>
-<p>g) Set the “Thank you page”. A page where your users will be redirected after the booking process is completed. Use this page to provide further informations to your users.</p>
-<p>Our plugin on activation creates a news page named “Thank you for booking” but you are free to select the page you want using the option select.</p>
+<p>c) <strong>Booking days in advance</strong><br>
+Set how many hours or days in advance users can book for an appointment. You need to set a minimum and a maximum time-range.</p>
+<p>d) <strong>Online booking available on:</strong><br>
+Set your salon’s timetable specifying the days and the time range when the bookings are allowed. The default settings expect your salon is open from monday to friday and from 8:00 to 13:00 and from 13:00 to 20:00.<br>
+Feel free to adjust these settings in order to fit your needs. 
+Our plug in allows you to set multiple booking rules using “Add new” button.</p>
+<p>e) <strong>Disable online booking</strong> <br>
+      This 
+is an option that you can use when you need to turn-off the online booking system. You can even set a custom message for your users.</p>
+<p>f) <strong>Booking confirmation<br>
+</strong> This option allows you to decide on single booking approval. On default settings this option is disabled and every single booking is automatically approved by the system. We recommend to leave this option disabled.</p>
+<p>g) <strong>Thank you page</strong> <br>
+      Set a page where your users will be redirected after the booking process is completed. Use this page to provide further information to your users. <br>
+      Our plugin on activation creates a new page named “Thank you for booking” but you are free to select the page you want using the option select.</p>
 <p><strong>When finish click “Update Settings” button</strong></p>
 <p>&nbsp;</p>
 <h3><em>Step 4</em></h3>
 <p><strong>Go to “Salon Settings” &#8211; &gt; “Payments” tab</strong></p>
 <p>Here you can decide if enabling online payments.</p>
-<p>a) Check the “Enable online payments” if you want your users need to pay in advance using a PayPal account.</p>
-<p>b) Enable “pay later” option if you want to give users the option to pay once in your salon.</p>
-<p>c) If you want to test PayPal payment you can enable the “PayPal Sandbox” mode. Remember to disable this option once you are ready to go live.</p>
+<p>a) Check the “<strong>Enable online payments</strong>” option if you want your users to pay in advance using a PayPal account.</p>
+<p>b) Enable “<strong>pay later</strong>” option if you want to give users the option to pay once they arrive in your salon.</p>
+<p>c) If you want to test PayPal payments you can enable the “<strong>PayPal Sandbox</strong>” mode. Remember to disable this option once you are ready to go live.</p>
 <p>&nbsp;</p>
 <p><strong>When finish click “Update Settings” button</strong></p>
 <p>&nbsp;</p>
@@ -45,28 +61,34 @@ This setting affects the number of bookings allowed for the same  date/time.</p>
 <p><strong>Go to “Salon” &#8211; &gt; “Services”</strong></p>
 <p>Now you need to create the services you want to be bookable through the online booking system.</p>
 <p>On activation this plugin creates three demo services. You can delete them or modify.</p>
-<p>a) To create a new service click on “Add Service” button.<br />
-b) Set the service name<br />
-c) Set the service price<br />
-d) Check “Secondary” option if you want the service being displayed in the “Step 3” of the booking process.<br />
-Use this option for “secondary” services, those services that you consider “optional” or “extra”.<br />
-e) you can set those days where this service is not available &#8211; leave it blank if the service is always available within your general timetable.<br />
-f) provide a short description for your service.<br />
-g) click on “Publish” button.</p>
+<p>a) To create a new service click on “<strong>Add Service</strong>” button.<br />
+      <br>
+      b) Set the service name<br />
+      <br>
+      c) Set the service price<br />
+      <br>
+      d) Check “<strong>Secondary</strong>” option if you want the service being displayed in the “Step 3” of the booking process.<br />
+      Use this option for “secondary” services, those services that you consider “optional” or “extra”.<br />
+      <br>
+      e) You can even set those days when this service is not available &#8211; leave it blank if the service is always available within your general timetable.<br />
+      <br>
+      f) provide a short description for your service.<br />
+      <br>
+g) click on “<strong>Publish</strong>” button.</p>
 <p>Create as many services as you need.</p>
 <p>&nbsp;</p>
 <h3><strong><em>Step 6</em></strong></h3>
 <p><strong>The booking page</strong></p>
 <p>This page is where the booking form will be displayed.<br />
-Once activated this plugin creates a page named “Booking” placing a shortcode <strong><code>[\salon]</code></strong> inside the text editor.</p>
+Once activated this plugin creates a page named “<strong>Booking</strong>” placing a shortcode <strong><code>[\salon]</code></strong> inside the text editor.</p>
 <p>You are free to place the shortcode inside another page if you want.</p>
 <p>&nbsp;</p>
 <p><strong>Other notes:</strong></p>
-<p>This plugin give final users the possibility to select only that dates and time that are available for bookings.</p>
-<p>This plugin make a real-time control over your settings (e.g. booking rules) and the upcoming confirmed reservations in order to display available booking dates.</p>
-<p>Every time a user complete a reservation an email will be sent to the website administrator and to the user with the details of the reservation.</p>
+<p>This plugin give final users the possibility to select only that dates and time that are actually available for bookings. <br>
+This plugin make a real-time control over your settings (e.g. booking rules) and the upcoming confirmed reservations in order to display available booking dates.</p>
+<p>Every time a user complete a reservation an email will be sent to the web site administrator and to the user with the details of the reservation.</p>
 <p>&nbsp;</p>
-<p>For any further information please go to our <strong><a href="http://salon.wordpresschef.it/" target="_blank">official support website.</a></strong></p>
+<p>For any further information please go to our <strong><a href="http://salon.wordpresschef.it/" target="_blank">official Salon Booking Plugin website.</a></strong></p>
 <p>If you have problems please use our <strong><a href="http://plugins.wordpresschef.it/forums/" target="_blank">official support forum.</a></strong></p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
