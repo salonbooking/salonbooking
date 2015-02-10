9937b8fbb90228ed7a0b26c95cd138207acb8115
Dimitri Grassi <contact@wordpresschef.it>
2015-02-10 09:26:36 +0000

    tab_homepage.php edited online with Bitbucket

--- views/settings/tab_homepage.php
+++ views/settings/tab_homepage.php
@@ -1,6 +1,6 @@
 <div class="sln-tab" id="sln-tab-general">
     <div class="row">
-        <p><?php _e('Use this function to restore all the original settings. All your actual settings will be lost. This operation can\'t be undone.', 'sln'); ?></p>
+        <p><?php _e('Use this option to restore all the original settings. All your actual settings will be lost. <br />This operation can\'t be undone.', 'sln'); ?></p>
         <br/> 
         <button type="submit" class="btn btn-warning" name="reset-settings" value="reset"
         onClick="return confirm('Do you really want to reset?');"
