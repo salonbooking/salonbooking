fde7638af9b9f61e9d15fe3175b878e25b48d36f
Dimitri Grassi <contact@wordpresschef.it>
2015-02-10 09:59:15 +0000

    tab_payments.php edited online with Bitbucket

--- views/settings/tab_payments.php
+++ views/settings/tab_payments.php
@@ -2,9 +2,11 @@
     <div class="row">
         <div class="col-md-4 col-sm-6">
             <?php $this->row_input_checkbox('pay_enabled', __('Enable online payments', 'sln')); ?>
+            <p><?php _e('Allow users to pay in advance using PayPal.', 'sln') ?></p>
         </div>
         <div class="col-md-4 col-sm-6">
-            <?php $this->row_input_checkbox('pay_cash', __('Enable "pay later" option', 'sln')); ?>
+            <?php $this->row_input_checkbox('pay_cash', __('Enable "Pay later" option', 'sln')); ?>
+            <p><?php _e('Give users the option to pay once they are at your salon.', 'sln') ?></p>
         </div>
   </div>
 
@@ -25,6 +27,7 @@
         <div class="col-md-4 col-sm-6">
             <div class="form-group">
                 <?php $this->row_input_checkbox('pay_paypal_test', __('Enable paypal sandbox', 'sln')); ?>
+                <p><?php _e('Check this option to test PayPal payments<br /> using your PayPal Sandbox account.', 'sln') ?></p>
             </div>
         </div>
     </div> 
