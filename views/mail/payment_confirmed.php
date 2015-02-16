c55ac8372d9aea63c4346d82e8549d0979dc6564
Dimitri Grassi <contact@wordpresschef.it>
2015-02-16 16:27:55 +0000

    payment_confirmed.php edited online with Bitbucket

--- views/mail/payment_confirmed.php
+++ views/mail/payment_confirmed.php
@@ -4,13 +4,13 @@
  * @var SLN_Wrapper_Booking       $booking
  */
 $data['to'] = $booking->getEmail();
-$data['subject'] = 'Your booking #'.$booking->getId().' has been confirmed';
+$data['subject'] = 'Payment for booking #'.$booking->getId().' has been confirmed';
 include dirname(__FILE__).'/_header.php';
 ?>
 <p ><?php _e('Dear', 'sln') ?>
     <strong><?php echo esc_attr($booking->getFirstname()) . ' ' . esc_attr($booking->getLastname()); ?></strong>
     <br/>
-    <?php _e('Payment for your booking is confirmed', 'sln') ?>
+    <?php _e('We received the payment for your booking.', 'sln') ?>
 </p>
 <?php
 include dirname(__FILE__).'/_footer.php';
