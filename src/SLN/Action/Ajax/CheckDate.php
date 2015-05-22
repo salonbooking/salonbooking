11a26e0cf3a8276f825c8f7113d4345ee7fa09b5
Server KF Informatica <kernelfolla@gmail.com>
2015-05-22 13:44:18 +0200

    fix bug datepicker

--- src/SLN/Action/Ajax/CheckDate.php
+++ src/SLN/Action/Ajax/CheckDate.php
@@ -106,7 +106,7 @@ class SLN_Action_Ajax_CheckDate extends SLN_Action_Ajax_Abstract
     public static function evalPickedDate($date){ 
         $date = explode(' ', $date);
         foreach(SLN_Func::getMonths(true) as $k => $v){
-            if(strcasecmp(substr($v, 0, 3),$date[1]) == 0){
+            if((strcasecmp(substr($v, 0, 3),$date[1]) == 0) || (strcasecmp($v, $date[1]) == 0)){
                 $ret = $date[2].'-'.($k < 10 ? '0'.$k : $k).'-'.$date[0];
                 return $ret;
             }
