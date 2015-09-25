80f1484f79dbfd80b9fe22e060324ba7c25d7f15
Dimitri Grassi <contact@wordpresschef.it>
2015-09-25 15:03:16 +0000

    _summary_content.php edited online with Bitbucket

--- views/mail/_summary_content.php
+++ views/mail/_summary_content.php
@@ -59,10 +59,10 @@ $showPrices = ($plugin->getSettings()->get('hide_prices') != '1')? true : false;
                 <td align="left" valign="top">&nbsp;</td>
               </tr>
               <tr>
-                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php if(isset($showPrices)){?><?php _e('Total amount', 'sln') ?><?php } ?></td>
+                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php if($showPrices){?><?php _e('Total amount', 'sln') ?><?php } ?></td>
               </tr>
               <tr>
-                <td height="36" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;"><?php if(isset($showPrices)){?><?php echo $plugin->format()->money($booking->getAmount()) ?><?php } ?></td>
+                <td height="36" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;"><?php if($showPrices){?><?php echo $plugin->format()->money($booking->getAmount()) ?><?php } ?></td>
               </tr>
               <tr>
                 <td height="28" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Status','sln')?></td>
