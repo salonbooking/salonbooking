2af429ebd514967d974cfd938f0f961172f81cff
francesco <francesco.l@icanlocalize.com>
2015-09-26 00:33:19 +0200

    google calendar api, googlescope class, settings tag gcalendar 2

---
+++ views/settings/tab_gcalendar.php
@@ -0,0 +1,15 @@
+<div class="sln-tab" id="sln-tab-general">
+    <div class="row">
+        <div class="col-md-10"><h3>Google Calendar</h3></div>
+        <div class="col-md-3 col-sm-4">
+            <?php $this->row_input_text('google_outh2_client_id', __('Google Client ID', 'sln')); ?>
+        </div>
+        <div class="col-md-3 col-sm-4">
+            <?php $this->row_input_text('google_outh2_client_secret', __('Google Client Secret', 'sln')); ?>
+        </div>
+        <div class="col-md-3 col-sm-4">
+            <?php $this->row_input_text('google_redirect_uri', __('Redirect URI', 'sln')); ?>
+        </div>
+    </div>
+
+</div>
