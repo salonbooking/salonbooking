8dafe7f536f4ef013bb1d3764bc5ee54537575b5
Server KF Informatica <kernelfolla@gmail.com>
2015-10-05 10:47:10 +0200

    new message booking

--- views/sms/summary.php
+++ views/sms/summary.php
@@ -3,7 +3,11 @@
  * @var SLN_Plugin          $plugin
  * @var SLN_Wrapper_Booking $booking
  */
-echo $booking->getId()
+echo
+    $booking->getFirstname() . ' ' . $booking->getLastname()
+    . __('has booked at ','sln') . $plugin->getSettings()->getSalonName() . __(' on ','sln')
     . ' ' . $plugin->format()->date($booking->getDate()) 
     . ' ' . $plugin->format()->time($booking->getTime())
-    . ' ' . $booking->getFirstname() . ' ' . $booking->getLastname();
+    . ' - ' .$booking->getId();
+
+
