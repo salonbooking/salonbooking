42dc96b751ef37d76897ea968917a640eca189fd
DimitriGrassi <contact@wordpresschef.it>
2015-11-20 11:31:52 +0100

    calendario al 100% rimossa la sidebar

--- views/admin/calendar.php
+++ views/admin/calendar.php
@@ -21,7 +21,7 @@ jQuery(function($){
 </div>
 <div class="clearfix"></div>
 <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-11">
 <div class="page-header pull-left">
 <h3></h3>
 </div>
@@ -38,17 +38,17 @@ jQuery(function($){
 	<button class="btn btn-warning active" data-calendar-view="month"><?php _e('Month')?></button>
 	<button class="btn btn-warning" data-calendar-view="week"><?php _e('Week')?></button>
 	<button class="btn btn-warning" data-calendar-view="day"><?php _e('Day')?></button>
-        <a href="<?php echo get_admin_url()?>edit.php?post_type=sln_booking" class="pull-right btn btn-warning" id="calendar-add-new">Add new</a>
+        <a href="<?php echo get_admin_url()?>edit.php?post_type=sln_booking" class="pull-right btn btn-warning" id="calendar-add-new">Add new booking</a>
             </div>
         </div>
         <div class="clearfix"></div>
         <div id="calendar"></div>
     </div>
-    <div class="col-md-3 sln-calendar-sidebar">
-        <h3>Bookings</h3>
-        <ul id="eventlist" class="nav nav-list"></ul>
-        <div class="legend">
-<h4><?php _e('Legend','sln')?></h4> 
+<div class="clearfix"></div>
+
+<div class="row">
+<div class="col-md-11">
+    <h4><?php _e('Bookings status legend','sln')?></h4> 
 <ul>
 <li><span class="pull-left event event-warning"></span><?php echo SLN_Enum_BookingStatus::getLabel(SLN_Enum_BookingStatus::PENDING) ?></li> 
 <li><span class="pull-left event event-success"></span><?php echo SLN_Enum_BookingStatus::getLabel(SLN_Enum_BookingStatus::PAID) ?> <?php _e('or','sln')?> <?php echo SLN_Enum_BookingStatus::getLabel(SLN_Enum_BookingStatus::CONFIRMED)?></li>
@@ -57,7 +57,5 @@ jQuery(function($){
 </ul>
 <div class="clearfix"></div>
         </div>
-    </div>
 </div>
-<div class="clearfix"></div>
-
+</div>
