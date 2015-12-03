346ba04a19b3eeddcd1567a5f9617d5cbc803a41
alexvp <aprokaev@gmail.com>
2015-12-03 16:13:52 +0300

    columns adjusted for Assis/Services/Bookings

--- src/SLN/PostType/Service.php
+++ src/SLN/PostType/Service.php
@@ -2,28 +2,118 @@
 
 class SLN_PostType_Service extends SLN_PostType_Abstract
 {
+
     public function init()
     {
         parent::init();
 
         if (is_admin()) {
+            add_action('pre_get_posts', array($this, 'admin_posts_sort'));
+            add_action('wp_insert_post', array($this, 'wp_insert_post'));
             add_action('manage_' . $this->getPostType() . '_posts_custom_column', array($this, 'manage_column'), 10, 2);
             add_filter('manage_' . $this->getPostType() . '_posts_columns', array($this, 'manage_columns'));
             add_action('admin_head-post-new.php', array($this, 'posttype_admin_css'));
             add_action('admin_head-post.php', array($this, 'posttype_admin_css'));
+            add_action('admin_enqueue_scripts', array($this, 'load_scripts'));
+            add_action('wp_ajax_sln_service', array($this, 'ajax'));
         }
+    }
+
+    function admin_posts_sort($query)
+    {
+        global $pagenow, $post_type;
+
+        if (is_admin() && 'edit.php' == $pagenow && $post_type == SLN_Plugin::POST_TYPE_SERVICE) {
+            $query->set('nopaging', 'true');
+           // $query->set('meta_key', '_sln_service_order');
+            $query->set('meta_query', array(
+                'relation' => 'OR',
+                array(
+                    'key' => '_sln_service_order',
+                    'compare' => 'EXISTS'
+                ),
+                array(
+                    'key' => '_sln_service_order',
+                    'compare' => 'NOT EXISTS'
+                )
+            ));
+            $query->set('orderby', '_sln_service_order');
+            $query->set('order', 'ASC');
+        }
+    }
+
+    public function load_scripts()
+    {
+        wp_enqueue_script('jquery-ui-core');
+        wp_enqueue_script('jquery-ui-sortable');
+    }
+
+    public function wp_insert_post($post_id, $wp_error = false)
+    {
+        global $post_type;
+
+        if ($post_type == SLN_Plugin::POST_TYPE_SERVICE) {
+            if (!get_post_meta($post_id, '_sln_service_order', true)) {
+                $count_pages = wp_count_posts(SLN_Plugin::POST_TYPE_SERVICE);
+                $pos = $count_pages->publish + 1;
+                add_post_meta($post_id, '_sln_service_order', $pos, true);
+            }
+        }
+    }
+
+    public function ajax()
+    {
+        if (isset($_POST['method'])) {
+            $method = 'ajax_' . $_POST['method'];
+            if (method_exists($this, $method)) {
+                $this->$method();
+            }
+        }
+        die();
+    }
+
+    public function ajax_save_position()
+    {
+        parse_str($_POST['data'], $params);
+
+        if (!isset($params['positions']))
+            return;
+
+        foreach (explode(',', $params['positions']) as $item) {
+            list($post_id, $pos) = explode('_', $item);
+            update_post_meta($post_id, '_sln_service_order', $pos);
+        }
+    }
+
+    public function ajax_save_cat_position()
+    {
+        global $wpdb;
+        parse_str($_POST['data'], $params);
+
+        if (!isset($params['positions']))
+            return;
+
+        update_option('sln_service_category_order', $params['positions']);
     }
 
     public function manage_columns($columns)
     {
-
-        return array_merge(
-            $columns,
-            array(
-                'service_duration' => __('Duration', 'sln'),
-                'service_price'    => __('Price', 'sln')
-            )
+        $new_columns = array(
+            'cb' => $columns['cb'],
+            'title' => $columns['title'],
+            'service_duration' => __('Duration', 'sln'),
+            'service_price' => __('Price', 'sln'),
+            'taxonomy-sln_service_category' => $columns['taxonomy-sln_service_category'],
+            'sln_days_off' => __('Days off', 'sln'),
         );
+
+//        return array_merge(
+//            $columns, array(
+//            'service_duration' => __('Duration', 'sln'),
+//            'service_price' => __('Price', 'sln')
+//            )
+//        );
+        return $new_columns;
     }
 
     public function manage_column($column, $post_id)
@@ -38,6 +128,16 @@ class SLN_PostType_Service extends SLN_PostType_Abstract
                 break;
             case 'service_price' :
                 echo $this->getPlugin()->format()->money(get_post_meta($post_id, '_sln_service_price', true));
+                break;
+            case 'sln_days_off' :
+                $days = SLN_Func::getDays();
+                $new_days = array();
+                foreach (get_post_meta($post_id) as $key => $meta) {
+                    preg_match('#^_sln_service_notav_([0-9]+)$#', $key, $matches);
+                    if (isset($matches[1]))
+                        $new_days[] = $days[$matches[1]];
+                }
+                echo implode(', ', $new_days);
                 break;
         }
     }
@@ -57,30 +157,27 @@ class SLN_PostType_Service extends SLN_PostType_Abstract
         global $post, $post_ID;
 
         $messages[$this->getPostType()] = array(
-            0  => '', // Unused. Messages start at index 1.
-            1  => sprintf(
+            0 => '', // Unused. Messages start at index 1.
+            1 => sprintf(
                 __('Service updated.', 'sln')
             ),
-            2  => '',
-            3  => '',
-            4  => __('Service updated.', 'sln'),
-            5  => isset($_GET['revision']) ? sprintf(
-                __('Service restored to revision from %s', 'sln'),
-                wp_post_revision_title((int)$_GET['revision'], false)
-            ) : false,
-            6  => sprintf(
+            2 => '',
+            3 => '',
+            4 => __('Service updated.', 'sln'),
+            5 => isset($_GET['revision']) ? sprintf(
+                    __('Service restored to revision from %s', 'sln'), wp_post_revision_title((int) $_GET['revision'], false)
+                ) : false,
+            6 => sprintf(
                 __('Service published.', 'sln')
             ),
-            7  => __('Service saved.', 'sln'),
-            8  => sprintf(
+            7 => __('Service saved.', 'sln'),
+            8 => sprintf(
                 __('Service submitted.', 'sln')
             ),
-            9  => sprintf(
+            9 => sprintf(
                 __(
-                    'Service scheduled for: <strong>%1$s</strong>. ',
-                    'sln'
-                ),
-                date_i18n(__('M j, Y @ G:i', 'restaurant'), strtotime($post->post_date))
+                    'Service scheduled for: <strong>%1$s</strong>. ', 'sln'
+                ), date_i18n(__('M j, Y @ G:i', 'restaurant'), strtotime($post->post_date))
             ),
             10 => sprintf(
                 __('Service draft updated.', 'sln')
@@ -94,32 +191,32 @@ class SLN_PostType_Service extends SLN_PostType_Abstract
     protected function getPostTypeArgs()
     {
         return array(
-            'public'              => true,
-            'publicly_queryable'  => true,
+            'public' => true,
+            'publicly_queryable' => true,
             'exclude_from_search' => true,
-            'show_in_menu'        => 'salon',
-            'rewrite'             => false,
-            'supports'            => array(
+            'show_in_menu' => 'salon',
+            'rewrite' => false,
+            'supports' => array(
                 'title',
                 'excerpt',
                 'thumbnail',
                 'revisions',
             ),
-            'labels'              => array(
-                'name'               => __('Services', 'sln'),
-                'singular_name'      => __('Service', 'sln'),
-                'menu_name'          => __('Salon', 'sln'),
-                'name_admin_bar'     => __('Salon Service', 'sln'),
-                'all_items'          => __('Services', 'sln'),
-                'add_new'            => __('Add Service', 'sln'),
-                'add_new_item'       => __('Add New Service', 'sln'),
-                'edit_item'          => __('Edit Service', 'sln'),
-                'new_item'           => __('New Service', 'sln'),
-                'view_item'          => __('View Service', 'sln'),
-                'search_items'       => __('Search Services', 'sln'),
-                'not_found'          => __('No services found', 'sln'),
+            'labels' => array(
+                'name' => __('Services', 'sln'),
+                'singular_name' => __('Service', 'sln'),
+                'menu_name' => __('Salon', 'sln'),
+                'name_admin_bar' => __('Salon Service', 'sln'),
+                'all_items' => __('Services', 'sln'),
+                'add_new' => __('Add Service', 'sln'),
+                'add_new_item' => __('Add New Service', 'sln'),
+                'edit_item' => __('Edit Service', 'sln'),
+                'new_item' => __('New Service', 'sln'),
+                'view_item' => __('View Service', 'sln'),
+                'search_items' => __('Search Services', 'sln'),
+                'not_found' => __('No services found', 'sln'),
                 'not_found_in_trash' => __('No services found in trash', 'sln'),
-                'archive_title'      => __('Services Archive', 'sln'),
+                'archive_title' => __('Services Archive', 'sln'),
             )
         );
     }
@@ -128,6 +225,7 @@ class SLN_PostType_Service extends SLN_PostType_Abstract
     {
         global $post_type;
         if ($post_type == SLN_Plugin::POST_TYPE_SERVICE) {
+
             ?>
             <style type="text/css">
                 #post-preview, #view-post-btn,
@@ -136,7 +234,7 @@ class SLN_PostType_Service extends SLN_PostType_Abstract
                     display: none;
                 }
             </style>
-        <?php
+            <?php
         }
     }
 }
