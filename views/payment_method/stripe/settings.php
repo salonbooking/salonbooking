452bfb4cb5598becbe8b0e18ec97fc467ce4ed99
Dimitri Grassi <contact@wordpresschef.it>
2015-12-14 16:05:46 +0000

    settings.php edited online with Bitbucket

--- views/payment_method/stripe/settings.php
+++ views/payment_method/stripe/settings.php
@@ -1,14 +1,14 @@
 
-    <div class="col-xs-12"><h2 class="sln-box-title">Stripe account informations</h2></div>
+    <div class="col-xs-12"><h2 class="sln-box-title"><?php _e('Stripe account informations','sln');?></h2></div>
         <div class="col-xs-12 col-sm-4 sln-input--simple">
             <?php $adminSettings->row_input_text('pay_stripe_apiKey', __('Enter your Stripe api key', 'sln')); ?>
-            <p class="sln-input-help">Mauris semper hendrerit erat, in consectetur arcu eleifend at.</p>
+            <p class="sln-input-help"><?php _e('-','sln');?></p>
         </div>
         <div class="col-xs-12 col-sm-4 sln-input--simple">
             <?php $adminSettings->row_input_text('pay_stripe_apiKeyPublic', __('Enter your Stripe publishable api key', 'sln')); ?>
-        <p class="sln-input-help">Mauris semper hendrerit erat, in consectetur arcu eleifend at.</p>
+        <p class="sln-input-help"><?php _e('-','sln');?></p>
         </div>
         <div class="col-xs-12 col-sm-4 sln-box-maininfo  align-top">
-            <p class="sln-input-help">Donec orci lacus, euismod euismod luctus sed, rhoncus in tellus. Mauris tempus arcu ut luctus venenatis.</p>
+            <p class="sln-input-help"><?php _e('To use this payment method you need an account with Stripe.','sln');?></p>
         </div>
 
