03e57c338576a4bbf022f459eff1b7794059eeca
Alexander <Alexander Mamutov>
2015-12-16 16:28:38 +0300

    less,css for dialog and admin email

--- views/mail/booking_rated.php
+++ views/mail/booking_rated.php
@@ -12,8 +12,8 @@ $data['subject'] = __('Booking was rated','sln');
 include dirname(__FILE__).'/_header.php';
 ?>
 
-	<tr>
-		<td height="105" valign="middle" bgcolor="#f2f2f2" style="border-bottom:2px solid #fff;">
+	<tr style="font-family: Arial, Helvetica, sans-serif; color: #888;">
+		<td height="105" valign="middle" bgcolor="#f2f2f2">
 			<p style="margin-left: 49px; margin-right: 49px;"><?php _e('Dear Administrator,','sln') ?></p>
 			<p style="margin-left: 49px; margin-right: 49px;">
 				<?php
@@ -24,10 +24,11 @@ include dirname(__FILE__).'/_header.php';
 				echo ' ' . $plugin->getSettings()->get('gen_name') . '.';
 				?>
 			</p>
+			<hr style="border: solid 1px #fff; margin: 0 16px;">
 		</td>
 	</tr>
 
-	<tr>
+	<tr style="font-family: Arial, Helvetica, sans-serif; color: #888;">
 		<td height="105" valign="middle" bgcolor="#f2f2f2">
 			<table width="502" border="0" align="left" cellpadding="0" cellspacing="0"  style="margin-left: 49px; margin-right: 49px;">
 				<tbody>
@@ -45,9 +46,9 @@ include dirname(__FILE__).'/_header.php';
 						<p style="padding-left: 20px;">
 							<a href="<?php echo esc_url(add_query_arg(array('p' => $booking->getId()), admin_url('edit-comments.php'))); ?>"style="
 										display: inline-block;
-									    padding: 6px 12px;
+									    padding: 16px 12px;
 									    margin-bottom: 0;
-									    font-size: 14px;
+									    font-size: 12px;
 									    font-weight: 400;
 									    line-height: 1.42857143;
 									    text-align: center;
