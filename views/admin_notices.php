fba0f340e531762c44fdbf4e1d73dc16ab18ad18
Nico Vece <me@nicovece.com>
2016-01-08 10:47:27 +0100

    SBP-81 cambiare la classe degli alert di errore - ora l'errore esce in un alert "verde" border color su  .settings-error

--- views/admin_notices.php
+++ views/admin_notices.php
@@ -1,4 +1,4 @@
-<div id="sln-setting-error" class="updated settings-error">
+<div id="sln-setting-error" class="updated settings-error error">
     <h3><?php _e('Salon booking plugin has been activated','salon-booking-system') ?></h3>
 
     <?php _e('<p>A new <strong>"Salon"</strong> section has been created into the left sidebar<br/>There you can find <strong>"Services", "Bookings", "Settings"</strong> sections<br/>We have also created "services" and "bookings" for demo purpose. Feel free to delete them<br/>Go to "Salon > Settings" section in order to set your own salon booking system</p>','salon-booking-system');?>
