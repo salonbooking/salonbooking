d8aaed58d7868f223bf44ca9586bf20484a292e1
q <crapty94@gmail.com>
2016-02-08 14:58:03 +0300

    added 'Customer' page

--- src/SLN/Admin/Customers.php
+++ src/SLN/Admin/Customers.php
@@ -7,17 +7,18 @@ class SLN_Admin_Customers {
 	protected $plugin;
 	protected $settings;
 	public $settings_page = '';
-	public function __construct(SLN_Plugin $plugin)
-	{
+	public function __construct(SLN_Plugin $plugin) {
 		$this->plugin   = $plugin;
 		$this->settings = $plugin->getSettings();
-		add_action('admin_menu', array($this, 'admin_menu'), 10 );
-		add_filter('manage_salon_page_salon-customers_columns', array($this, 'users_columns'));
+		add_action('admin_menu', array($this, 'admin_menu'), 10);
+
+		if (!isset($_REQUEST['id'])) {
+			add_filter('manage_salon_page_salon-customers_columns', array($this, 'users_columns'));
+		}
 
 	}
 
-	public function admin_menu()
-	{
+	public function admin_menu() {
 		$this->settings_page = add_submenu_page(
 			'salon',
 			__('Salon Customers', 'salon-booking-system'),
@@ -26,37 +27,62 @@ class SLN_Admin_Customers {
 			'salon-customers',
 			array($this, 'show')
 		);
-
 	}
 
-	public function show()
-	{
-		if (!empty($_REQUEST['id'])) {
-			$this->show_customer_page();
+	public function show() {
+		if (isset($_REQUEST['id'])) {
+			$this->show_customer_page($_REQUEST['id']);
 		}
 		else {
 			$this->show_customers();
 		}
 	}
 
-	public function show_customer_page() {
+	public function show_customer_page($user_id) {
 
-		$user_id = $_REQUEST['id'];
+		$customer = new SLN_Wrapper_Customer(new WP_User($user_id));
 
+		if (!empty($user_id) && $customer->isEmpty()) {
+			wp_redirect(get_edit_user_link($user_id));
+			exit;
+		}
+
+		if (isset($_POST['save'])) {
+			if (empty($_POST['id'])) {
+				$user_id = wp_create_user($_POST['sln_customer']['user_email'], wp_generate_password(), $_POST['sln_customer']['user_email']);
+			}
+			else {
+				$user_id = $_POST['id'];
+			}
+
+			$_POST['sln_customer']['ID'] = $user_id;
+			$_POST['sln_customer']['role'] = SLN_Plugin::USER_ROLE_CUSTOMER;
+			wp_update_user($_POST['sln_customer']);
+
+			foreach ($_POST['sln_customer_meta'] as $k => $value) {
+				update_user_meta($user_id, $k, $value);
+			}
+
+			wp_redirect(self::get_edit_customer_link($user_id));
+			exit;
+		}
+
+		$customer = new SLN_Wrapper_Customer(new WP_User($user_id));
 
 		echo $this->plugin->loadView(
 				'admin/_customer',
 				array(
-						'x' => 'x'
+						'customer' => $customer,
+						'new_link' => self::get_edit_customer_link(0),
 				)
 		);
 	}
 
 	public function show_customers() {
-		if ( empty($_REQUEST) ) {
-			$referer = '<input type="hidden" name="wp_http_referer" value="'. esc_attr( wp_unslash( $_SERVER['REQUEST_URI'] ) ) . '" />';
-		} elseif ( isset($_REQUEST['wp_http_referer']) ) {
-			$redirect = remove_query_arg(array('wp_http_referer', 'updated', 'delete_count'), wp_unslash( $_REQUEST['wp_http_referer'] ) );
+		if (empty($_REQUEST)) {
+			$referer = '<input type="hidden" name="wp_http_referer" value="'. esc_attr(wp_unslash($_SERVER['REQUEST_URI'])) . '" />';
+		} elseif (isset($_REQUEST['wp_http_referer'])) {
+			$redirect = remove_query_arg(array('wp_http_referer', 'updated', 'delete_count'), wp_unslash($_REQUEST['wp_http_referer']));
 			$referer = '<input type="hidden" name="wp_http_referer" value="' . esc_attr($redirect) . '" />';
 		} else {
 			$redirect = 'admin.php';
@@ -65,10 +91,13 @@ class SLN_Admin_Customers {
 
 		$table = new SLN_Admin_Customers_List();
 
-		switch ( $table->current_action() ) {
+		switch ($table->current_action()) {
+			case 'delete':
+
+				break;
 			default:
 				if (!empty($table->current_action())) {
-					if ( empty($_REQUEST['users']) ) {
+					if (empty($_REQUEST['users'])) {
 						wp_redirect($redirect);
 						exit();
 					}
@@ -77,8 +106,8 @@ class SLN_Admin_Customers {
 					exit();
 				}
 				else {
-					if ( !empty($_GET['_wp_http_referer']) ) {
-						wp_redirect( remove_query_arg( array( '_wp_http_referer', '_wpnonce'), wp_unslash( $_SERVER['REQUEST_URI'] ) ) );
+					if (!empty($_GET['_wp_http_referer'])) {
+						wp_redirect(remove_query_arg(array('_wp_http_referer', '_wpnonce'), wp_unslash($_SERVER['REQUEST_URI'])));
 						exit;
 					}
 				}
@@ -89,8 +118,8 @@ class SLN_Admin_Customers {
 		echo $this->plugin->loadView(
 				'admin/customers',
 				array(
-						'x' => 'x',
-						'table' => $table,
+						'new_link' => self::get_edit_customer_link(0),
+						'table'    => $table,
 				)
 		);
 	}
---
+++ views/admin/_customer.php
@@ -0,0 +1,115 @@
+<div class="wrap sln-bootstrap" id="sln-salon--admin">
+	<h1>
+		<?php /** @var SLN_Wrapper_Customer $customer */ ?>
+		<?php _e($customer->isEmpty() ? 'New Customer' : 'Edit Customer', 'salon-booking-system') ?>
+		<?php /** @var string $new_link */ ?>
+		<a href="<?php echo $new_link; ?>" class="page-title-action"><?php echo esc_html_x('Add Customer', 'salon-booking-system'); ?></a>
+	</h1>
+	<br>
+
+	<form method="post">
+		<input type="hidden" name="id" id="id" value="<?php echo $customer->getId(); ?>">
+		<div class="row">
+			<div class="col-xs-10 col-md-10 col-lg-10 col-sm-10 postbox">
+				<h3><?php _e('Customer details', 'salon-booking-system') ?></h3>
+				<div class="row inside">
+					<div class="col-xs-3 col-md-3 col-lg-3 col-sm-3">
+						<div class="form-group sln_meta_field">
+							<label for="_sln_customer_first_name"><?php _e('First name', 'salon-booking-system') ?></label>
+							<input type="text" name="sln_customer[first_name]" id="_sln_customer_first_name" value="<?php echo $customer->get('first_name'); ?>" class="form-control">
+						</div>
+					</div>
+					<div class="col-xs-3 col-md-3 col-lg-3 col-sm-3">
+						<div class="form-group sln_meta_field">
+							<label for="_sln_customer_last_name"><?php _e('Last name', 'salon-booking-system') ?></label>
+							<input type="text" name="sln_customer[last_name]" id="_sln_customer_last_name" value="<?php echo $customer->get('last_name'); ?>" class="form-control">
+						</div>
+					</div>
+					<div class="col-xs-3 col-md-3 col-lg-3 col-sm-3">
+						<div class="form-group sln_meta_field">
+							<label for="_sln_customer_email"><?php _e('E-mail', 'salon-booking-system') ?></label>
+							<input type="text" name="sln_customer[user_email]" id="_sln_customer_email" value="<?php echo $customer->get('user_email'); ?>" class="form-control" required>
+						</div>
+					</div>
+					<div class="col-xs-3 col-md-3 col-lg-3 col-sm-3">
+						<div class="form-group sln_meta_field">
+							<label for="_sln_customer_sln_phone"><?php _e('Phone', 'salon-booking-system') ?></label>
+							<input type="text" name="sln_customer_meta[_sln_phone]" id="_sln_customer_sln_phone" value="<?php echo $customer->get('_sln_phone'); ?>" class="form-control">
+						</div>
+					</div>
+				</div>
+
+				<div class="row inside">
+					<div class="col-xs-6 col-md-6 col-lg-6 col-sm-6">
+						<div class="form-group sln_meta_field">
+							<label for="_sln_customer_sln_address"><?php _e('Address', 'salon-booking-system') ?></label>
+							<textarea type="text" name="sln_customer_meta[_sln_address]" id="_sln_customer_sln_address" class="form-control"><?php echo $customer->get('_sln_address'); ?></textarea>
+						</div>
+					</div>
+				</div>
+
+				<div class="row inside">
+					<div class="col-xs-6 col-md-6 col-lg-6 col-sm-6">
+						<div class="form-group sln_meta_field">
+							<label for="_sln_customer_sln_personal_note"><?php _e('Personal note', 'salon-booking-system') ?></label>
+							<textarea type="text" name="sln_customer_meta[_sln_personal_note]" id="_sln_customer_sln_personal_note" class="form-control"><?php echo $customer->get('_sln_personal_note'); ?></textarea>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-1 col-md-1 col-lg-1 col-sm-1 col-md-offset-1">
+				<div class="row">
+					<div class="col-xs-12 col-md-12 col-lg-12 col-sm-12">
+						<div class="form-group sln_meta_field">
+							<input type="submit" name="save" value="<?php _e($customer->isEmpty() ? 'Publish' : 'Update', 'salon-booking-system'); ?>" class="button button-primary" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-10 col-md-10 col-lg-10 col-sm-10 postbox">
+				<h3><?php _e('Booking history', 'salon-booking-system') ?></h3>
+				<div class="row inside">
+					<div class="col-xs-2 col-md-2 col-lg-2 col-sm-2">
+						<h4><?php _e('Total reservations', 'salon-booking-system') ?></h4>
+					<span>
+						<?php
+						$count = $customer->getCountOfReservations();
+						echo $count;
+						?>
+					</span>
+					</div>
+					<div class="col-xs-2 col-md-2 col-lg-2 col-sm-2">
+						<h4><?php _e('Customer value', 'salon-booking-system') ?></h4>
+						<span><?php echo SLN_Plugin::getInstance()->getSettings()->getCurrencySymbol() . ' ' . $customer->getCustomerValue(); ?></span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php if ($count): ?>
+			<div class="row">
+				<div class="col-xs-12 col-md-12 col-lg-12 col-sm-12 postbox">
+				<?php
+
+				$_GET['post_type'] = SLN_Plugin::POST_TYPE_BOOKING;
+				$_GET['author'] = $customer->getId();
+				get_current_screen()->add_option('post_type', SLN_Plugin::POST_TYPE_BOOKING);
+				get_current_screen()->id = 'edit-sln_booking';
+				get_current_screen()->post_type = SLN_Plugin::POST_TYPE_BOOKING;
+
+				/** @var SLN_Admin_Customers_BookingsList $wp_list_table */
+				$wp_list_table = new SLN_Admin_Customers_BookingsList();
+
+				$wp_list_table->prepare_items();
+
+				$wp_list_table->display();
+				?>
+				</div>
+			</div>
+		<?php endif; ?>
+	</form>
+
+</div>
