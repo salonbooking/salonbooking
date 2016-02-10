5e64e96a1770ba8f84b2d219bba2e872d272491d
q <crapty94@gmail.com>
2016-02-10 09:35:04 +0300

    fix notice view & update script

--- src/SLN/Action/Updates/sln-update-2.2.1.php
+++ src/SLN/Action/Updates/sln-update-2.2.1.php
@@ -7,7 +7,8 @@ $users = $wpdb->get_col($query);
 $users = array_unique($users);
 foreach ($users as $userId) {
 	$user = new WP_User($userId);
-	if (empty(array_intersect($user->roles, array('administrator', SLN_Plugin::USER_ROLE_STAFF, SLN_Plugin::USER_ROLE_CUSTOMER)))) {
+	$f = array_intersect($user->roles, array('administrator', SLN_Plugin::USER_ROLE_STAFF, SLN_Plugin::USER_ROLE_CUSTOMER));
+	if (empty($f)) {
 		wp_update_user(array(
 			'ID' => $userId,
 			'role' => SLN_Plugin::USER_ROLE_CUSTOMER,
--- views/notice/html_notice_update.php
+++ views/notice/html_notice_update.php
@@ -1,5 +1,8 @@
 <div id="sln-setting-success" class="updated settings-success success">
-	<p><?php _e('<strong>Salon Data Update Required</strong> - We just need to update your install to the latest version','salon-booking-system') ?></p>
+	<p>
+		<strong><?php _e('Salon Data Update Required','salon-booking-system') ?></strong> -
+		<?php _e('We just need to update your install to the latest version','salon-booking-system') ?>
+	</p>
 
 	<p>
 		<a href="<?php echo esc_url( add_query_arg( 'do_update_sln', 'true', admin_url( 'admin.php?page=salon-settings' ) ) ); ?>"
