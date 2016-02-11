ac40c6d520b2f7806526d7cf380dba6b297e4b81
Dimitri Grassi <contact@wordpresschef.it>
2016-02-11 10:05:49 +0000

    remind.php edited online with Bitbucket

--- views/sms/remind.php
+++ views/sms/remind.php
@@ -3,7 +3,10 @@
  * @var SLN_Plugin          $plugin
  * @var SLN_Wrapper_Booking $booking
  */
-echo __('Your booking is coming','salon-booking-system'). $booking->getId()
-    . ' ' . $plugin->format()->date($booking->getDate()) 
-    . ' ' . $plugin->format()->time($booking->getTime())
-    . ' ' . $booking->getFirstname() . ' ' . $booking->getLastname();
+echo 
+__('Hi ','salon-booking-system') . $booking->getFirstname() . ' ' . $booking->getLastname()
+
+. __('don\'t forget your reservation at ','salon-booking-system'). $plugin->getSettings()->getSalonName()
+. __(' on ','salon-booking-system') . $plugin->format()->date($booking->getDate()) 
+. __(' at ','salon-booking-system') . $plugin->format()->time($booking->getTime())
+. __(' Booking ID ','salon-booking-system') .$booking->getId();
