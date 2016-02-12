5d1f814261f90bdbd7ac648e9b5101138f905f55
Server KF Informatica <kernelfolla@gmail.com>
2016-02-12 12:35:01 +0100

    changed label

--- src/SLN/PaymentMethod/Paypal.php
+++ src/SLN/PaymentMethod/Paypal.php
@@ -22,7 +22,7 @@ class SLN_PaymentMethod_Paypal extends SLN_PaymentMethod_Abstract
             } elseif ($action == 'notify') {
                 $this->processIpn($op[1]);
             } elseif ($action == 'cancel') {
-                return __('Payment canceled', 'salon-booking-system');
+                return __('Your payment has not been completed', 'salon-booking-system');
             } else {
                 throw new Exception('payment method operation not managed');
             }
