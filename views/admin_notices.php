ecdae81b49db749ba9b501ff4ada24eb35153e63
Dimitri Grassi <contact@wordpresschef.it>
2016-02-17 09:34:03 +0000

    admin_notices.php edited online with Bitbucket

--- views/admin_notices.php
+++ views/admin_notices.php
@@ -1,7 +1,7 @@
 <div id="sln-setting-error" class="updated settings-success success">
     <h3><?php _e('Congratulation! Salon Booking is now active.','salon-booking-system') ?></h3>
 
-    <?php _e('<p>A new <strong>"Salon"</strong> section has been created into the left sidebar<br/>Three new pages has been created: "Booking", "Booking my account" and "Thanjk you page"<br/>We have also added some "services" and "bookings" for demo purpose. Feel free to delete them<br/>Go to "Salon > Settings" section in order to set your own salon booking system</p>','salon-booking-system');?>
+    <?php _e('<p>A new <strong>"Salon"</strong> section has been created into the left sidebar<br/>Three new pages has been created: "Booking", "Booking my account" and "Thank you page"<br/>We have also added some "services" and "bookings" for demo purpose. Feel free to delete them<br/>Go to <a href="/wp-admin/admin.php?page=salon"><strong>"Salon > Settings"</strong></a>section to setup your Salon Booking System</p>','salon-booking-system');?>
 
     <p>
         <strong><a class="dismiss-notice"
