66ae937e542b1c0ebff10a89c4783cdd2d50ddd1
Server KF Informatica <kernelfolla@gmail.com>
2016-02-29 13:09:41 +0100

    importati stili da multishop

--- views/metabox/attendant.php
+++ views/metabox/attendant.php
@@ -1,80 +1,67 @@
 <?php
 $helper->showNonce($postType);
 ?>
-
-<?php
-$days = SLN_Func::getDays();
-?>
-<div class="row">
-
-    <div class="col-xs-3 col-md-3 col-lg-3 col-sm-3  attendants-notavailable-h">
-        <?php
-        $helper->showFieldtext(
-            $helper->getFieldName($postType, 'email'),
-            __('E-mail', 'salon-booking-system'),
-            $attendant->getEmail()
-        ); ?>
+<div class="row sln-service-price-time">
+    <div class="col-sm-6 col-md-3 form-group sln-input--simple">
+            <label for="_sln_attendant_email">E-mail</label>
+            <input type="text" name="_sln_attendant_email" id="_sln_attendant_email" value="<?php echo $attendant->getEmail() ?>" class="form-control">
     </div>
-    <div class="col-xs-3 col-md-3 col-lg-3 col-sm-3  attendants-notavailable-h">
-        <?php
-        $helper->showFieldtext(
-            $helper->getFieldName($postType, 'phone'),
-            __('Phone', 'salon-booking-system'),
-            $attendant->getPhone()
-        ); ?>
+    <div class="col-sm-6 col-md-3 form-group sln-select">
+            <label for="_sln_attendant_phone">Telefono cell.</label>
+            <input type="text" name="_sln_attendant_phone" id="_sln_attendant_phone" value="<?php echo $attendant->getPhone() ?>" class="form-control">
     </div>
-    <div class="col-xs-6 col-md-6 col-lg-6 col-sm-6 sln-select-wrapper">
-        <label><?php _e('Services', 'salon-booking-system'); ?></label>
-        <select class="sln-select" multiple="multiple" data-placeholder="<?php _e('Select or search one or more services', 'salon-booking-system')?>"
-                name="_sln_attendant_services[]" id="_sln_attendant_services">
-            <?php foreach ($plugin->getServices() as $service) : ?>
-                <option
-                    class="red"
-                    value="sln_attendant_services_<?php echo $service->getId() ?>"
-                    data-price="<?php echo $service->getPrice(); ?>"
-                    <?php echo $attendant->hasService($service) ? 'selected="selected"' : '' ?>
-                ><?php echo $service->getName(); ?>
-                    (<?php echo $plugin->format()->money($service->getPrice()) ?>)
-                </option>
-            <?php endforeach ?>
-        </select>
+    <div class="col-sm-12 col-md-6 form-group sln-select sln-select--multiple">
+            <label>Servizi</label>
+            <select class="sln-select select2-hidden-accessible" multiple="multiple" data-placeholder="<?php _e('Select or search one or more services')?>"
+                    name="_sln_booking_services[]" id="_sln_booking_services" tabindex="-1" aria-hidden="true">
+                <?php foreach ($plugin->getServices() as $service) : ?>
+                    <option
+                        class="red"
+                        value="sln_attendant_services_<?php echo $service->getId() ?>"
+                        data-price="<?php echo $service->getPrice(); ?>"
+                        <?php echo $attendant->hasService($service) ? 'selected="selected"' : '' ?>
+                        ><?php echo $service->getName(); ?>
+                        (<?php echo $plugin->format()->money($service->getPrice()) ?>)
+                    </option>
+                <?php endforeach ?>
+            </select>
     </div>
-
-</div>
-<h3><?php _e('Not available on','salon-booking-system'); ?></h3>
-<div class="row">
-    <div class="col-md-12 attendants-notavailable">
-        <?php foreach ($days as $k => $day) { ?>
-            <label>
-                <?php SLN_Form::fieldCheckbox(
-                    $helper->getFieldName($postType, 'notav_' . $k),
-                    $attendant->getNotAvailableOn($k)
-                ) ?>
-                <?php echo substr($day, 0, 3) ?>
-            </label>
-        <?php } ?>
-    </div>
-    <div class="col-xs-6 col-md-3 col-lg-2 col-sm-3  attendants-notavailable-h">
-        <label>
-            <?php echo __('From', 'salon-booking-system') ?>
-            <?php SLN_Form::fieldTime(
-                $helper->getFieldName($postType, 'notav_from'),
-                $attendant->getNotAvailableFrom()
-            ) ?>
-        </label>
-    </div>
-    <div class="col-xs-6 col-md-3 col-lg-2 col-sm-3  attendants-notavailable">
-        <label>
-            <?php echo __('To', 'salon-booking-system') ?>
-            <?php SLN_Form::fieldTime(
-                $helper->getFieldName($postType, 'notav_to'),
-                $attendant->getNotAvailableTo()
-            ) ?>
-        </label>
-    </div>
-    <div class="col-xs-12 col-md-12 col-lg-12 col-sm-12  attendants-notavailable-h">
-    <em><?php _e('Leave this option blank if you want this assistant available for every hour each day', 'salon-booking-system') ?></em>
 </div>
 
+<div class="sln-box--sub sln-booking-rules row">
+    <div class="col-xs-6">
+        <h2 class="sln-box-title"><?php _e('Not available on','salon-booking-system'); ?></h2>
+        <small><?php _e('Leave this option blank if you want this attendant available for every hour each day', 'salon-booking-system') ?></small>
+    </div>
+    <div class="sln-clear"></div>
+    <div id="sln-booking-rules-wrapper">
+        <div class="col-xs-12 sln-booking-rule">
+            <h6 class="sln-fake-label">Not available days checked and green.</h6>
+            <div class="sln-checkbutton-group">
+        <?php foreach (SLN_Func::getDays() as $k => $day) : ?>
+            <div class="sln-checkbutton">
+                    <?php SLN_Form::fieldCheckboxButton(
+                        $helper->getFieldName($postType, 'notav_' . $k),
+                        ($attendant->getNotAvailableOn($k) ? 1 : null),
+                        $label = substr($day, 0, 3)
+                    ) ?>
+            </div>
+        <?php endforeach ?>
+            <div class="clearfix"></div>
+        </div>
+        <div class="row">
+           <div class="col-xs-12 col-md-8 sln-slider-wrapper">
+              <div class="sln-slider">
+                  <div class="sliders_step1 col col-slider"><div class="slider-range ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"><div class="ui-slider-range ui-widget-header ui-corner-all" style="left: 0%; width: 50%;"></div><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 0%;"></span><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 50%;"></span></div></div>
+                  <div class="col col-time"><span class="slider-time-from">0:00</span> to <span class="slider-time-to">12:00</span>
+                      <?php echo SLN_Form::fieldText($helper->getFieldName($postType, 'notav_from'), $attendant->getNotAvailableFrom()->format('H:i'), array('attrs' => array('class' => 'slider-time-input-from hidden'))) ?>
+                      <?php echo SLN_Form::fieldText($helper->getFieldName($postType, 'notav_to'), $attendant->getNotAvailableTo()->format('H:i'), array('attrs' => array('class' => 'slider-time-input-to hidden'))) ?>
+                  </div>
+                  <div class="clearfix"></div>
+             </div>
+           </div>
+         </div>
+       </div>
+    </div>
 </div>
 <div class="sln-clear"></div>
--- views/metabox/service.php
+++ views/metabox/service.php
@@ -1,84 +1,86 @@
 <?php
 $helper->showNonce($postType);
 ?>
+
 <div class="row sln-service-price-time">
-    <div class="col-xs-6 col-md-2 col-sm-2 col-lg-2">
-        <div class="form-group">
+<!-- default settings -->
+    <div class="col-sm-6 col-md-3 form-group sln-input--simple">
             <label><?php _e('Price', 'salon-booking-system') . ' (' . $settings->getCurrencySymbol() . ')' ?></label>
             <?php SLN_Form::fieldText($helper->getFieldName($postType, 'price'), $service->getPrice()); ?>
-        </div>
     </div>
-    <div class="col-xs-6 col-md-3 col-sm-2 col-lg-2">
-        <div class="form-group">
-
+    <div class="col-sm-6 col-md-3 form-group sln-select">
             <label><?php _e('Unit per hour', 'salon-booking-system'); ?></label>
             <?php SLN_Form::fieldNumeric($helper->getFieldName($postType, 'unit'), $service->getUnitPerHour()); ?>
-        </div>
     </div>
-    <div class="col-xs-6 col-md-3 col-sm-2 col-lg-2">
-        <div class="form-group">
-
+    <div class="col-sm-6 col-md-3 form-group sln-select">
             <label><?php _e('Duration', 'salon-booking-system'); ?></label>
             <?php SLN_Form::fieldTime($helper->getFieldName($postType, 'duration'), $service->getDuration()); ?>
-        </div>
     </div>
-    <div class="col-xs-6 col-md-4 col-sm-2 col-lg-2">
-        <div class="form-group">
-            <label><?php _e('Secondary', 'salon-booking-system'); ?></label>
-            <?php SLN_Form::fieldCheckbox($helper->getFieldName($postType, 'secondary'), $service->isSecondary()) ?>
-            <br/><em><?php _e('Select this if you want this service considered as secondary level service','salon-booking-system'); ?></em>
-        </div>
-    </div>
+    <div class="col-sm-6 col-md-3 form-group sln-checkbox">
+                         <?php SLN_Form::fieldCheckbox($helper->getFieldName($postType, 'secondary'), $service->isSecondary()) ?>
+            <label for="_sln_service_secondary"><?php _e('Secondary', 'salon-booking-system'); ?></label>
+                    <p><?php _e('Select this if you want this service considered as secondary level service','salon-booking-system'); ?></p>
+            </div>
+    <div class="sln-clear"></div>
+</div>
+
+<?php $m_attendant_enabled = $settings->get('m_attendant_enabled'); ?>
+<?php if($m_attendant_enabled): ?>
+<div class="row sln-service-price-time">
     <div class="col-xs-6 col-md-6 col-sm-4 col-lg-4">
         <div class="form-group">
-            <?php $m_attendant_enabled = $settings->get('m_attendant_enabled'); ?>
             <?php $attrs = $m_attendant_enabled ? array() : array('disabled'=>'disabled') ?>
             <label><?php _e('Execution Order', 'salon-booking-system'); ?></label>
             <?php SLN_Form::fieldNumeric($helper->getFieldName($postType, 'exec_order'), $service->getExecOrder(), array('min' => 1, 'max' => 10, 'attrs' => $attrs)) ?>
+<?php /*
             <?php if (!$m_attendant_enabled) {
                 SLN_Form::fieldText($helper->getFieldName($postType, 'exec_order'), $service->getExecOrder(), array('type' => 'hidden'));
             } ?>
+*/
+?>
             <br/><em><?php _e('Set this option if you have enabled "Multiple assistants selection". Use a number to give this service an order of execution compared to the other services.','salon-booking-system'); ?></em>
             <br/><em><?php _e('Consider that this option will affect the availability of your staff members that you have associated th this service.','salon-booking-system'); ?></em>
         </div>
     </div>
     <div class="sln-clear"></div>
 </div>
-<h3><?php _e('Not available on','salon-booking-system'); ?></h3>
-<?php
-$days = SLN_Func::getDays();
-?>
-<div class="row">
-    <div class="col-md-12 services-notavailable">
-        <?php foreach ($days as $k => $day) { ?>
-            <label>
-                <?php SLN_Form::fieldCheckbox(
-                    $helper->getFieldName($postType, 'notav_' . $k),
-                    $service->getNotAvailableOn($k)
-                ) ?>
-                <?php echo substr($day, 0, 3) ?>
-            </label>
-        <?php } ?>
-    </div>
-    <div class="col-xs-6 col-md-3 col-lg-2 col-sm-3  services-notavailable-h">
-        <label>
-            <?php echo __('From', 'salon-booking-system') ?>
-            <?php SLN_Form::fieldTime(
-                $helper->getFieldName($postType, 'notav_from'),
-                $service->getNotAvailableFrom()
-            ) ?>
-        </label>
-    </div>
-    <div class="col-xs-6 col-md-3 col-lg-2 col-sm-3  services-notavailable-h">
-        <label>
-            <?php echo __('To', 'salon-booking-system') ?>
-            <?php SLN_Form::fieldTime(
-                $helper->getFieldName($postType, 'notav_to'),
-                $service->getNotAvailableTo()
-            ) ?>
-        </label>
+<?php endif ?>
 
+<div class="sln-box--sub sln-booking-rules row">
+    <div class="col-xs-6">
+        <h2 class="sln-box-title"><?php _e('Not available on','salon-booking-system'); ?></h2>
+        <small><?php _e('Leave this option blank if you want this service available for every hour each day', 'salon-booking-system') ?></small>
+    </div>
+    <div class="sln-clear"></div>
+    <div id="sln-booking-rules-wrapper">
+        <div class="col-xs-12 sln-booking-rule">
+            <h6 class="sln-fake-label">Not available days checked and green.</h6>
+            <div class="sln-checkbutton-group">
+        <?php foreach (SLN_Func::getDays() as $k => $day) : ?>
+            <div class="sln-checkbutton">
+                    <?php SLN_Form::fieldCheckboxButton(
+                        $helper->getFieldName($postType, 'notav_' . $k),
+                        ($service->getNotAvailableOn($k) ? 1 : null),
+                        $label = substr($day, 0, 3)
+                    ) ?>
+            </div>
+        <?php endforeach ?>
+            <div class="clearfix"></div>
+        </div>
+        <div class="row">
+           <div class="col-xs-12 col-md-8 sln-slider-wrapper">
+              <div class="sln-slider">
+                  <div class="sliders_step1 col col-slider"><div class="slider-range ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"><div class="ui-slider-range ui-widget-header ui-corner-all" style="left: 0%; width: 50%;"></div><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 0%;"></span><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 50%;"></span></div></div>
+                  <div class="col col-time"><span class="slider-time-from">0:00</span> to <span class="slider-time-to">12:00</span>
+                      <?php echo SLN_Form::fieldText($helper->getFieldName($postType, 'notav_from'), $service->getNotAvailableFrom()->format('H:i'), array('attrs' => array('class' => 'slider-time-input-from hidden'))) ?>
+                      <?php echo SLN_Form::fieldText($helper->getFieldName($postType, 'notav_to'), $service->getNotAvailableTo()->format('H:i'), array('attrs' => array('class' => 'slider-time-input-to hidden'))) ?>
+                  </div>
+                  <div class="clearfix"></div>
+             </div>
+           </div>
+         </div>
+       </div>
     </div>
 </div>
-<em><?php _e('Leave this option blank if you want this service available for every hour each day', 'salon-booking-system') ?></em>
+
 <div class="sln-clear"></div>
