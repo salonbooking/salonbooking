c7b37de2a6622ab2763df01e970c37195154b3fc
Dimitri Grassi <contact@wordpresschef.it>
2016-03-01 19:07:46 +0000

    admin_end.php edited online with Bitbucket

--- views/trial/admin_end.php
+++ views/trial/admin_end.php
@@ -1,5 +1,5 @@
 <div id="sln-setting-error" class="updated error">
-    <h3><?php _e('Salon booking plugin is expired','salon-booking-system') ?></h3>
+    <p><?php _e('Salon booking plugin is expired','salon-booking-system') ?></p>
 
     <?php _e('<p><a href="http://plugins.wordpresschef.it/downloads/salon-booking-wordpress-plugin/">Please upgrade Salon Booking to a Premium version</a></p>','salon-booking-system');?>
 </div>
