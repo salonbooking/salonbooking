d195c0628fa33d007f40086cf4652ee410eec803
q <crapty94@gmail.com>
2016-03-25 16:51:00 +0300

    'Holiday rules' update script was fixed

--- src/SLN/Action/Updates/sln-update-2.3.1.php
+++ src/SLN/Action/Updates/sln-update-2.3.1.php
@@ -3,9 +3,29 @@
 // START UPDATE HOLIDAYS RULES
 $holidays = SLN_Plugin::getInstance()->getSettings()->get('holidays');
 $holidays = !empty($holidays) ? $holidays : array();
-foreach($holidays as &$holidayData) {
-	$holidayData['from_date'] = SLN_Func::evalPickedDate($holidayData['from_date']);
-	$holidayData['to_date']   = SLN_Func::evalPickedDate($holidayData['to_date']);
+foreach($holidays as $k => &$holidayData) {
+	try {
+		$from_date = SLN_Func::evalPickedDate($holidayData['from_date']);
+	} catch(Exception $e) {
+		if (!strtotime($holidayData['from_date'])) {
+			unset($holidays[$k]);
+			continue;
+		}
+		$from_date = date('Y-m-d', strtotime($holidayData['from_date']));
+	}
+	$holidayData['from_date'] = $from_date;
+
+	try {
+		$to_date = SLN_Func::evalPickedDate($holidayData['to_date']);
+	} catch(Exception $e) {
+		if (!strtotime($holidayData['to_date'])) {
+			unset($holidays[$k]);
+			continue;
+		}
+		$to_date = date('Y-m-d', strtotime($holidayData['to_date']));
+	}
+	$holidayData['to_date'] = $to_date;
+
 	$holidayData['from_time'] = date('H:i', strtotime($holidayData['from_time']));
 	$holidayData['to_time']   = date('H:i', strtotime($holidayData['to_time']));
 }
