b6425be951d48d0a60a01365efe2c88d9b8cab4d
q <crapty94@gmail.com>
2016-03-29 15:57:59 +0400

    text changes

--- src/SLN/Helper/Availability.php
+++ src/SLN/Helper/Availability.php
@@ -173,7 +173,7 @@ class SLN_Helper_Availability
             ) {
                 SLN_Plugin::addLog(__CLASS__.sprintf(' - attendant %s by date(%s) busy',$attendant,$time->format('Ymd H:i')));
                 return array(
-                    sprintf(__('The attendant for %s is currently full', 'salon-booking-system'), $time->format('H:i'))
+                    sprintf(__('This assistant is full at %s', 'salon-booking-system'), $time->format('H:i'))
                 );
             }
         }
@@ -220,7 +220,7 @@ class SLN_Helper_Availability
             if (empty($attendants)) {
                 SLN_Plugin::addLog(__CLASS__.sprintf(' - all of the assistants for service %s by date(%s) are busy',$service,$time->format('Ymd H:i')));
                 return array(
-                    __('No one of the assistants for this service is available at ', 'salon-booking-system') . $time->format('H:i')
+                    __('No assistants available for this service at ', 'salon-booking-system') . $time->format('H:i')
                 );
             }
 
