6344ea54fae6e0fdc78d70937a90ae4683c7d966
q <crapty94@gmail.com>
2016-04-05 14:28:17 +0400

    Custom wordpress plugin update notice

--- src/SLN/Action/Install.php
+++ src/SLN/Action/Install.php
@@ -121,4 +121,53 @@ class SLN_Action_Install
     {
         return get_page_by_title($title, null, $post_type) ? true : false;
     }
+
+    /**
+     * Show plugin changes. Code adapted from W3 Total Cache.
+     */
+    public static function inPluginUpdateMessage( $args ) {
+
+        $transient_name = 'sln_upgrade_notice_' . $args['Version'];
+
+        if ( false === ( $upgrade_notice = get_transient( $transient_name ) ) ) {
+            $response = wp_safe_remote_get( 'https://plugins.svn.wordpress.org/salon-booking-system/trunk/readme.txt' );
+
+            if ( ! is_wp_error( $response ) && ! empty( $response['body'] ) ) {
+                $upgrade_notice = self::parseUpdateNotice( $response['body'] );
+                set_transient( $transient_name, $upgrade_notice, DAY_IN_SECONDS );
+            }
+        }
+
+        echo wp_kses_post( $upgrade_notice );
+    }
+
+    /**
+     * Parse update notice from readme file.
+     * @param  string $content
+     * @return string
+     */
+    private static function parseUpdateNotice( $content ) {
+        // Output Upgrade Notice
+        $matches        = null;
+        $regexp         = '~==\s*Upgrade Notice\s*==\s*=\s*(.*)\s*=(.*)(=\s*' . preg_quote( SLN_Plugin::getInstance()->getSettings()->getVersion() ) . '\s*=|$)~Uis';
+        $upgrade_notice = '';
+
+        if ( preg_match( $regexp, $content, $matches ) ) {
+            $version = trim( $matches[1] );
+            $notices = (array) preg_split('~[\r\n]+~', trim( $matches[2] ) );
+
+            if ( version_compare( SLN_Plugin::getInstance()->getSettings()->getVersion(), $version, '<' ) ) {
+
+                $upgrade_notice .= '<div class="sln_plugin_upgrade_notice">';
+
+                foreach ( $notices as $index => $line ) {
+                    $upgrade_notice .= wp_kses_post( preg_replace( '~\[([^\]]*)\]\(([^\)]*)\)~', '<a href="${2}">${1}</a>', $line ) );
+                }
+
+                $upgrade_notice .= '</div> ';
+            }
+        }
+
+        return wp_kses_post( $upgrade_notice );
+    }
 }
