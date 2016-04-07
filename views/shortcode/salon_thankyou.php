5f22ec3af3123298ada2ce140d1b0e5c3295eebb
Nico Vece <me@nicovece.com>
2016-04-07 20:00:21 +0200

    SBP-153 - thank you step on active confirmation

--- views/shortcode/salon_thankyou.php
+++ views/shortcode/salon_thankyou.php
@@ -36,11 +36,11 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
         </div>
 
     <?php else: ?>
-    <div class="row sln-thankyou--okbox">
+    <div class="row sln-thankyou--okbox <?php if($confirmation): ?> sln-bkg--attention<?php else : ?> sln-bkg--ok<?php endif ?>">
         <div class="col-md-12">
         <h1 class="sln-icon-wrapper"><?php echo $confirmation ? __('Your booking is pending', 'salon-booking-system') : __('Your booking is confirmed', 'salon-booking-system') ?>
-            <?php if($confirmation): ?> 
-                <i class="c glyphicon glyphicon-time"></i>
+            <?php if($confirmation): ?>
+                <i class="sln-icon sln-icon--time"></i>
             <?php else : ?>
                 <i class="sln-icon sln-icon--checked--square"></i>
             <?php endif ?>
@@ -58,9 +58,9 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
         <p><strong><?php _e('You will receive a confirmation of your booking by email.','salon-booking-system' )?></strong></p>
         <p><?php echo sprintf(__('If you don\'t receive any news from us or you need to change your reservation please call the %s or send an e-mail to %s', 'salon-booking-system'), $plugin->getSettings()->get('gen_phone'),  $plugin->getSettings()->get('gen_email') ? $plugin->getSettings()->get('gen_email') : get_option('admin_email') ); ?>
             <?php echo $plugin->getSettings()->get('gen_phone') ?></p>
-        <p class="aligncenter"><a href="<?php echo $confirmUrl ?>"
+        <?php /*<p class="aligncenter"><a href="<?php echo $confirmUrl ?>"
              data-salon-toggle="direct" data-salon-data="<?php echo $ajaxData.'&mode=confirm' ?>"
-             class="btn btn-primary"><?php _e('Complete','salon-booking-system') ?></a></p>
+             class="btn btn-primary"><?php _e('Complete','salon-booking-system') ?></a></p>*/ ?>
 <?php /*
         <p class="aligncenter"><a href="<?php echo home_url() ?>" class="btn btn-primary"><?php _e('Back to home','salon-booking-system') ?></a></p>
 */ ?>
@@ -68,11 +68,10 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
         <p><?php echo sprintf(__('If you need to change your reservation please call the <strong>%s</strong> or send an e-mail to <strong>%s</strong>', 'salon-booking-system'), $plugin->getSettings()->get('gen_phone'),  $plugin->getSettings()->get('gen_email') ? $plugin->getSettings()->get('gen_email') : get_option('admin_email') ); ?>
             <?php echo $plugin->getSettings()->get('phone') ?>
         </p>
-        </div>
-
-            <div id="sln-notifications"></div>
     <!-- form actions -->
 <?php endif ?>
+        </div>
+        <div id="sln-notifications"></div>
 <?php endif ?>
     </div>
     <div class="col-md-4 sln-form-actions-wrapper sln-input--action">
@@ -97,7 +96,7 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
                 <h4><?php _e('Or', 'salon-booking-system') ?></h4>
             </div>
         <?php elseif(!$paymentMethod) : ?>
-            <div class="col-md-8 pull-right">
+            <div class="col-sm-6 col-md-8 pull-right">
                 <a  href="<?php echo $laterUrl ?>" class="sln-btn sln-btn--emphasis sln-btn--big sln-btn--fullwidth"
                     <?php if($ajaxEnabled): ?>
                         data-salon-data="<?php echo $ajaxData.'&mode=later' ?>" data-salon-toggle="direct"
@@ -127,11 +126,11 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
         </div>
 
     <?php else: ?>
-    <div class="row sln-thankyou--okbox">
+    <div class="row sln-thankyou--okbox <?php if($confirmation): ?> sln-bkg--attention<?php else : ?> sln-bkg--ok<?php endif ?>">
         <div class="col-md-12">
         <h1 class="sln-icon-wrapper"><?php echo $confirmation ? __('Your booking is pending', 'salon-booking-system') : __('Your booking is confirmed', 'salon-booking-system') ?>
             <?php if($confirmation): ?> 
-                <i class="c glyphicon glyphicon-time"></i>
+                <i class="sln-icon sln-icon--time"></i>
             <?php else : ?>
                 <i class="sln-icon sln-icon--checked--square"></i>
             <?php endif ?>
@@ -149,10 +148,10 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
         <p><strong><?php _e('You will receive a confirmation of your booking by email.','salon-booking-system' )?></strong></p>
         <p><?php echo sprintf(__('If you don\'t receive any news from us or you need to change your reservation please call the %s or send an e-mail to %s', 'salon-booking-system'), $plugin->getSettings()->get('gen_phone'),  $plugin->getSettings()->get('gen_email') ? $plugin->getSettings()->get('gen_email') : get_option('admin_email') ); ?>
             <?php echo $plugin->getSettings()->get('gen_phone') ?></p>
-        <p class="aligncenter"><a href="<?php echo $confirmUrl ?>"
+   <?php /*     <p class="aligncenter"><a href="<?php echo $confirmUrl ?>"
              data-salon-toggle="direct" data-salon-data="<?php echo $ajaxData.'&mode=confirm' ?>"
              class="btn btn-primary"><?php _e('Complete','salon-booking-system') ?></a></p>
-<?php /*
+
         <p class="aligncenter"><a href="<?php echo home_url() ?>" class="btn btn-primary"><?php _e('Back to home','salon-booking-system') ?></a></p>
 */ ?>
 <?php else : ?>
@@ -192,8 +191,8 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
                 </div>
             </div>
         <?php elseif(!$paymentMethod) : ?>
-        <div class="form-actions row">
-            <div class="col-md-8 pull-right">
+        <div class="sln-form-actions sln-payment-actions row">
+            <div class="col-sm-6 col-md-8 pull-right">
                 <a  href="<?php echo $laterUrl ?>" class="sln-btn sln-btn--emphasis sln-btn--big sln-btn--fullwidth"
                     <?php if($ajaxEnabled): ?>
                         data-salon-data="<?php echo $ajaxData.'&mode=later' ?>" data-salon-toggle="direct"
@@ -223,11 +222,11 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
         </div>
 
     <?php else: ?>
-    <div class="row sln-thankyou--okbox">
+    <div class="row sln-thankyou--okbox <?php if($confirmation): ?> sln-bkg--attention<?php else : ?> sln-bkg--ok<?php endif ?>">
         <div class="col-md-12">
         <h1 class="sln-icon-wrapper"><?php echo $confirmation ? __('Your booking is pending', 'salon-booking-system') : __('Your booking is confirmed', 'salon-booking-system') ?>
             <?php if($confirmation): ?> 
-                <i class="c glyphicon glyphicon-time"></i>
+                <i class="sln-icon sln-icon--time"></i>
             <?php else : ?>
                 <i class="sln-icon sln-icon--checked--square"></i>
             <?php endif ?>
@@ -240,15 +239,15 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
         </div>
     </div>
 <?php $ppl = false; ?>
-<div class="sln-alert sln-alert--warning">
+<div class="sln-alert sln-alert--warning <?php if($confirmation) : ?> sln-alert--topicon<?php endif ?>">
 <?php if($confirmation) : ?>
         <p><strong><?php _e('You will receive a confirmation of your booking by email.','salon-booking-system' )?></strong></p>
         <p><?php echo sprintf(__('If you don\'t receive any news from us or you need to change your reservation please call the %s or send an e-mail to %s', 'salon-booking-system'), $plugin->getSettings()->get('gen_phone'),  $plugin->getSettings()->get('gen_email') ? $plugin->getSettings()->get('gen_email') : get_option('admin_email') ); ?>
             <?php echo $plugin->getSettings()->get('gen_phone') ?></p>
-        <p class="aligncenter"><a href="<?php echo $confirmUrl ?>"
+<?php /*        <p class="aligncenter"><a href="<?php echo $confirmUrl ?>"
              data-salon-toggle="direct" data-salon-data="<?php echo $ajaxData.'&mode=confirm' ?>"
              class="btn btn-primary"><?php _e('Complete','salon-booking-system') ?></a></p>
-<?php /*
+
         <p class="aligncenter"><a href="<?php echo home_url() ?>" class="btn btn-primary"><?php _e('Back to home','salon-booking-system') ?></a></p>
 */ ?>
 <?php else : ?>
@@ -288,7 +287,7 @@ $size = SLN_Enum_ShortcodeStyle::getSize($style);
         </div>
         <?php elseif(!$paymentMethod) : ?>
         <div class="form-actions row">
-            <div class="col-md-8 pull-right">
+            <div class="col-sm-12 col-md-8 pull-right">
                 <a  href="<?php echo $laterUrl ?>" class="sln-btn sln-btn--emphasis sln-btn--big sln-btn--fullwidth"
                     <?php if($ajaxEnabled): ?>
                         data-salon-data="<?php echo $ajaxData.'&mode=later' ?>" data-salon-toggle="direct"
