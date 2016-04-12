ca50eb034f7365c4e316142c632086f1b43ca3cf
Marino Di Clemente <kernelfolla@gmail.com>
2016-04-12 10:58:48 +0200

    minor fix php 5.2

--- src/SLN/Repository/AbstractRepository.php
+++ src/SLN/Repository/AbstractRepository.php
@@ -4,5 +4,5 @@ abstract class SLN_Repository_AbstractRepository
 {
     abstract public function create($data = null);
     abstract public function getBindings();
-    abstract public function get($criteria = []);
+    abstract public function get($criteria = array());
 }
