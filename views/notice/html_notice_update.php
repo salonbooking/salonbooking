042cc417fbc8b72c66403feffeabe40b9fc9fbf6
Dimitri Grassi <contact@wordpresschef.it>
2016-04-15 08:17:24 +0000

    html_notice_update.php edited online with Bitbucket

--- views/notice/html_notice_update.php
+++ views/notice/html_notice_update.php
@@ -1,7 +1,7 @@
 <div id="sln-setting-success" class="updated settings-success success">
 	<p>
 		<strong><?php _e('Salon Data Update Required','salon-booking-system') ?></strong> -
-		<?php echo __('An update is required for this version. In case of problems you can use a “Rollback” option','salon-booking-system') ?>
+		<?php echo __('A database update is required for this version. Please make a back-up of your database before proceed.','salon-booking-system') ?>
 	</p>
 
 	<p>
