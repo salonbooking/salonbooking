faffdf2549e5fff2b43585c49d60cf26dafdeefe
Marino Di Clemente <kernelfolla@gmail.com>
2016-04-21 10:11:19 +0200

    SBP-183 Notification email - display only arriving time - remove time for each service

--- views/mail/_summary_details.php
+++ views/mail/_summary_details.php
@@ -1,65 +1,134 @@
 <?php
 $showPrices = !$plugin->getSettings()->isHidePrices();
 /** @var SLN_Wrapper_Booking $booking */
+$isMultipleAttSelection = $plugin->getSettings->get('m_attendant_enabled');
 ?>
 <table width="502" border="0" align="left" cellpadding="0" cellspacing="0">
-          <tbody><tr>
-            <td width="157" align="center" valign="top" style="border-right:2px solid #f2f2f2;">
-              <table width="306" border="0" align="center" cellpadding="0" cellspacing="0">
-              <tbody><tr>
-                <td align="left" valign="top">&nbsp;</td>
-              </tr>
-              <tr>
-                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Date', 'salon-booking-system')?></td>
-                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Time','salon-booking-system') ?></td>
-                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Service','salon-booking-system') ?></td>
-                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Attendant','salon-booking-system') ?></td>
-              </tr>
-              <?php $printDate = true; ?>
-              <?php foreach($booking->getBookingServices()->getItems() as $bookingService): ?>
-              <tr>
+    <tbody>
+    <tr>
+        <td width="157" align="center" valign="top" style="border-right:2px solid #f2f2f2;">
+<?php if ($isMultipleAttSelection) : ?>
+    <table width="306" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tbody><tr>
+            <td align="left" valign="top">&nbsp;</td>
+        </tr>
+        <tr>
+            <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Date', 'salon-booking-system')?></td>
+            <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Time','salon-booking-system') ?></td>
+            <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Service','salon-booking-system') ?></td>
+            <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Attendant','salon-booking-system') ?></td>
+        </tr>
+        <?php $printDate = true; ?>
+        <?php foreach($booking->getBookingServices()->getItems() as $bookingService): ?>
+            <tr>
                 <td height="36" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:15px; color:#666666; font-weight:bold;"><?php if ($printDate) {$printDate = false; echo $plugin->format()->date($bookingService->getStartsAt());} ?></td>
                 <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:15px; color:#666666; font-weight:bold;"><?php echo $plugin->format()->time($bookingService->getStartsAt()) ?></td>
                 <td height="20" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#666666; font-weight:bold;"><?php echo $bookingService->getService()->getName(); ?></td>
                 <td height="20" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#666666; font-weight:bold;"><?php echo $bookingService->getAttendant()->getName(); ?></td>
-              </tr>
-              <?php endforeach ?>
-              <tr>
+            </tr>
+        <?php endforeach ?>
+        <tr>
+            <td align="left" valign="top">&nbsp;</td>
+        </tr>
+        </tbody>
+    </table>
+<?php else: ?>
+    <table width="112" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tbody><tr>
+            <td align="left" valign="top">&nbsp;</td>
+        </tr>
+            <tr>
+                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Date', 'salon-booking-system')?></td>
+            </tr>
+            <tr>
+                <td height="36" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;"><?php echo $plugin->format()->date($booking->getDate()); ?></td>
+            </tr>
+            <tr>
+                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php echo _e('Time','salon-booking-system') ?></td>
+            </tr>
+            <tr>
+                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;"> <?php echo $plugin->format()->time($booking->getTime()) ?></td>
+            </tr>
+            <?php if($attendant = $booking->getAttendant()) :  ?>
+                <tr>
+                    <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php echo _e('Attendant','salon-booking-system') ?></td>
+                </tr>
+                <tr>
+                    <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;"> <?php echo $attendant->getName() ?></td>
+                </tr>
+            <?php endif ?>
+
+            <tr>
                 <td align="left" valign="top">&nbsp;</td>
-              </tr>
-            </tbody>
-            </table>
-            </td>
-
-            <td width="147" align="center" valign="top"><table width="112" border="0" align="center" cellpadding="0" cellspacing="0">
-              <tbody><tr>
-                <td align="left" valign="top">&nbsp;</td>
-              </tr>
-              <tr>
-                <td height="25" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php if($showPrices){?><?php _e('Total amount', 'salon-booking-system') ?><?php } ?></td>
-              </tr>
-              <tr>
-                <td height="36" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;"><?php if($showPrices){?><?php echo $plugin->format()->money($booking->getAmount()) ?><?php } ?></td>
-              </tr>
-              <tr>
-                <td height="28" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Status','salon-booking-system')?></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;">
-                    <?php echo SLN_Enum_BookingStatus::getLabel($booking->getStatus()) ?>
-                    <?php if( $booking->getDeposit() && $booking->hasStatus(SLN_Enum_BookingStatus::PAID) ){ ?>
-
-                        <span style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; font-weight:normal;"><br/>Deposit <?php echo $plugin->format()->money($booking->getDeposit()) ?></span>
-
-                    
-
-                    <?php } ?>
-                </td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">&nbsp;</td>
-              </tr>
+            </tr>
             </tbody></table></td>
-          </tr>
-        </tbody></table>
-       
+    <td width="194" align="center" valign="top" style="border-right:2px solid #f2f2f2;"><table width="155" border="0" cellspacing="0" cellpadding="0">
+            <tbody><tr>
+                <td align="left" valign="top">&nbsp;</td>
+            </tr>
+            <tr>
+                <td height="30" align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e('Services','salon-booking-system') ?></td>
+            </tr>
+            <?php foreach ($plugin->getServices() as $service) : ?>
+                <?php if ($booking->hasService($service)): ?>
+                    <tr>
+                        <td height="20" align="left" valign="top"
+                            style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#666666; font-weight:bold;"><?php echo $service->getName(
+                            ); ?></td>
+                    </tr>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <tr>
+                <td align="left" valign="top">&nbsp;</td>
+            </tr>
+            </tbody></table>
+<?php endif ?>
+        </td>
+        <td width="147" align="center" valign="top">
+            <table width="112" border="0" align="center" cellpadding="0" cellspacing="0">
+                <tbody>
+                <tr>
+                    <td align="left" valign="top">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="25" align="left" valign="top"
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php if ($showPrices) { ?><?php _e(
+                            'Total amount',
+                            'salon-booking-system'
+                        ) ?><?php } ?></td>
+                </tr>
+                <tr>
+                    <td height="36" align="left" valign="top"
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;"><?php if ($showPrices) { ?><?php echo $plugin->format(
+                        )->money($booking->getAmount()) ?><?php } ?></td>
+                </tr>
+                <tr>
+                    <td height="28" align="left" valign="top"
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#cc3333; font-weight:normal;"><?php _e(
+                            'Status',
+                            'salon-booking-system'
+                        ) ?></td>
+                </tr>
+                <tr>
+                    <td align="left" valign="top"
+                        style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#666666; font-weight:bold;">
+                        <?php echo SLN_Enum_BookingStatus::getLabel($booking->getStatus()) ?>
+                        <?php if ($booking->getDeposit() && $booking->hasStatus(SLN_Enum_BookingStatus::PAID)) { ?>
+
+                            <span
+                                style="font-size: 14px; font-family: Arial, Helvetica, sans-serif; font-weight:normal;"><br/>Deposit <?php echo $plugin->format(
+                                )->money($booking->getDeposit()) ?></span>
+
+
+                        <?php } ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" valign="top">&nbsp;</td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
