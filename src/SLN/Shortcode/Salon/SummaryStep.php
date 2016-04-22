cd30ead7f63d1b923c45d8c9728d94c9ac9f86fd
Server KF Informatica <kernelfolla@gmail.com>
2016-04-22 11:28:53 +0200

    fix double mail

--- src/SLN/Shortcode/Salon/SummaryStep.php
+++ src/SLN/Shortcode/Salon/SummaryStep.php
@@ -9,6 +9,7 @@ class SLN_Shortcode_Salon_SummaryStep extends SLN_Shortcode_Salon_Step
         if (!$bb->getLastBooking()) {
             $bb->set('note', SLN_Func::filter($values['note']));
             $bb->create();
+            /* fix double mail 
             if($this->getPlugin()->getSettings()->get('confirmation')){
                 $this->getPlugin()->sendMail(
                     'mail/summary',
@@ -19,6 +20,7 @@ class SLN_Shortcode_Salon_SummaryStep extends SLN_Shortcode_Salon_Step
                     array('booking' => $bb->getLastBooking())
                 );
             }
+            */
         }
 
         return true;
