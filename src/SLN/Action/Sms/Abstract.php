cfe0edaa52fcd321c06a781c71643d885ae160e4
Marino Di Clemente <kernelfolla@gmail.com>
2016-06-27 12:27:59 +0200

    fix #SBP-241

--- src/SLN/Action/Sms/Abstract.php
+++ src/SLN/Action/Sms/Abstract.php
@@ -11,29 +11,28 @@ abstract class SLN_Action_Sms_Abstract
 
     abstract public function send($to, $message);
 
-    protected function getAccount(){
+    protected function getAccount()
+    {
         return $this->plugin->getSettings()->get('sms_account');
     }
 
-    protected function getPassword(){
+    protected function getPassword()
+    {
         return $this->plugin->getSettings()->get('sms_password');
     }
 
-    protected function getFrom(){
+    protected function getFrom()
+    {
         return $this->plugin->getSettings()->get('sms_from');
     }
 
-    protected function processTo($to){
-        $s = $this->plugin->getSettings();
-        $prefix = $s->get('sms_prefix');
-        if($s->get('sms_trunk_prefix') && strpos($to,'0') === 0){
-            $to = substr($to,1);
-        }
-        //$prefix = str_replace('+','',$prefix);
-        $to = str_replace(' ','',$to);
-        return $prefix . $to;
+    protected function processTo($to)
+    {
+        $this->plugin->format()->phone($to);
     }
-    protected function createException($message, $code = 1000, $previous = null){
+
+    protected function createException($message, $code = 1000, $previous = null)
+    {
         throw new SLN_Action_Sms_Exception($message, $code, $previous);
     }
 }
