76cd19362dfeff7b8e0849e716b2dcb8be447d44
q <crapty94@gmail.com>
2016-09-21 11:10:01 +0400

    option added

--- views/settings/tab_checkout.php
+++ views/settings/tab_checkout.php
@@ -20,5 +20,30 @@ $plugin = SLN_Plugin::getInstance();
 				</div>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-sm-6 col-md-8 col-lg-4 form-group sln-select ">
+				<label for="salon_settings_services_count"><?php _e('Services to be booked simultaneously','salon-booking-system') ?></label>
+				<?php echo SLN_Form::fieldSelect(
+						'salon_settings[services_count]',
+						array(
+							''   => "No limits",
+							'1'  => "1",
+							'2'  => "2",
+							'3'  => "3",
+							'4'  => "4",
+							'5'  => "5",
+							'6'  => "6",
+							'7'  => "7",
+							'8'  => "8",
+							'9'  => "9",
+							'10' => "10",
+						),
+						$this->settings->get('services_count'),
+						array(),
+						true
+				) ?>
+				<p class="sln-input-help"><?php _e('Set this option if you want to limit the number of services bookable during a single reservation.','salon-booking-system');?></p>
+			</div>
+		</div>
 	</div>
 </div>
