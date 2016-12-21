f638792c404dac53ec3014597fc7f17522048bfa
q <crapty94@gmail.com>
2016-12-21 12:49:33 +0400

    Secondary display mode options implemented

--- src/SLN/Action/Ajax/CheckServicesAlt.php
+++ src/SLN/Action/Ajax/CheckServicesAlt.php
@@ -4,7 +4,10 @@ class SLN_Action_Ajax_CheckServicesAlt extends SLN_Action_Ajax_CheckServices
 {
     protected function innerInitServices($services, $merged, $newServices)
     {
-        $builder       = $this->bb;
+        $ret      = array();
+        $builder  = $this->bb;
+        $this->ah->setDate($this->bb->getDateTime());
+
         $mergeIds = array();
         foreach($merged as $s){
             $mergeIds[] = $s->getId();
@@ -18,18 +21,33 @@ class SLN_Action_Ajax_CheckServicesAlt extends SLN_Action_Ajax_CheckServices
 
         foreach ($this->getServices(true, true) as $service) {
             $error = '';
-            if ($servicesCount && (count($services) >= $servicesCount && ! in_array($service->getId(), $services))) {
-                $status = self::STATUS_ERROR;
-                $error  = sprintf(__('You can select up to %d items', 'salon-booking-system'), $servicesCount);
-            } elseif (in_array($service->getId(), $services)) {
-                $builder->addService($service);
-                $status = self::STATUS_CHECKED;
+            if (in_array($service->getId(), $services)) {
+                $bookingServices = SLN_Wrapper_Booking_Services::build(array_fill_keys($services, 0), $this->getDateTime());
+                $serviceErrors   = $this->ah->validateServiceFromOrder($service, $bookingServices);
+                if(empty($serviceErrors)) {
+                    $builder->addService($service);
+                    $status = self::STATUS_CHECKED;
+                }
+                else {
+                    unset($services[array_search($service->getId(), $services)]);
+                    $status = self::STATUS_ERROR;
+                    $error  = reset($serviceErrors);
+                }
             } else {
                 $status = self::STATUS_UNCHECKED;
             }
             $ret[$service->getId()] = array('status' => $status, 'error' => $error);
         }
         $builder->save();
+
+        $servicesErrors = $this->ah->checkEachOfNewServicesForExistOrder($services, $newServices, true);
+        foreach ($servicesErrors as $sId => $error) {
+            if (empty($error)) {
+                $ret[$sId] = array('status' => self::STATUS_UNCHECKED, 'error' => '');
+            } else {
+                $ret[$sId] = array('status' => self::STATUS_ERROR, 'error' => $error[0]);
+            }
+        }
 
         return $ret;
     }
