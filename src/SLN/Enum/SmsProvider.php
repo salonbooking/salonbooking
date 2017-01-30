3fe08ff55678e798c4c3fa73c30322e4ea91e05b
Marino Di Clemente <kernelfolla@gmail.com>
2017-01-30 15:22:48 +0100

    #SBP-377

--- src/SLN/Enum/SmsProvider.php
+++ src/SLN/Enum/SmsProvider.php
@@ -6,11 +6,11 @@ class SLN_Enum_SmsProvider
     private static $labels = array();
 
     private static $classes = array(
-        'fake' => 'SLN_Action_Sms_Fake',
-        'ip1smshttp' => 'SLN_Action_Sms_Ip1SmsHttp',
+        'fake'             => 'SLN_Action_Sms_Fake',
+        'ip1smshttp'       => 'SLN_Action_Sms_Ip1SmsHttp',
         'ip1smswebservice' => 'SLN_Action_Sms_Ip1SmsWebservice',
-        'twilio' => 'SLN_Action_Sms_Twilio',
-        'plivo' => 'SLN_Action_Sms_Plivo',
+        'twilio'           => 'SLN_Action_Sms_Twilio',
+        'plivo'            => 'SLN_Action_Sms_Plivo',
     );
 
     public static function toArray()
@@ -28,7 +28,7 @@ class SLN_Enum_SmsProvider
     }
 
     /**
-     * @param $key
+     * @param            $key
      * @param SLN_Plugin $plugin
      * @return SLN_Action_Sms_Abstract
      * @throws Exception
@@ -52,11 +52,18 @@ class SLN_Enum_SmsProvider
     public static function init()
     {
         self::$labels = array(
-            'fake' => __('test (sms code is sent by mail to the admin)', 'salon-booking-system'),
+            'fake'             => __('test (sms code is sent by mail to the admin)', 'salon-booking-system'),
             'ip1smswebservice' => 'ip1sms',
-            'twilio' => 'Twilio',
-            'plivo' => 'Plivo',
+            'twilio'           => 'Twilio',
+            'plivo'            => 'Plivo',
         );
+        do_action('sln.sms_provider.init');
+    }
+
+    public static function addService($key, $label, $class)
+    {
+        self::$labels[$key]  = $label;
+        self::$classes[$key] = $class;
     }
 }
 
