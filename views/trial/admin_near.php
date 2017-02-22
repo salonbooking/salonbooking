21b999b1f9695646d2509b14a1011c60e4954791
Dimitri Grassi <contact@wordpresschef.it>
2017-02-22 15:13:22 +0000

    admin_near.php edited online with Bitbucket

--- views/trial/admin_near.php
+++ views/trial/admin_near.php
@@ -1,6 +1,6 @@
 <div id="sln-setting-error" class="updated error">
-    <p><?php _e('You are going to reach the bookings limit for the Salon Booking free version','salon-booking-system') ?></p>
+    <p><?php _e('You are going to reach the bookings limit for the Salon Booking free version. <a href="http://salon.wordpresschef.it/salon-booking-plugin-pricing/" target="blank">Please upgrade Salon Booking to a PRO version</a>.','salon-booking-system') ?>
 
-    <?php _e('<p><a href="http://plugins.wordpresschef.it/downloads/salon-booking-wordpress-plugin/">Please upgrade Salon Booking to a Premium version</a></p>','salon-booking-system');?>
+    <p><?php _e('<strong>Do you want a 30% discount? <a href="http://salon.wordpresschef.it/invite-friends-get-30-discount-first-purchase/" target="blank">INVITE YOUR FRIENDS!</a></strong></p>','salon-booking-system');?></p>
 </div>
 
