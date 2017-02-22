199c1aa5f397d20989a62950c0c417ae9a4b9584
Dimitri Grassi <contact@wordpresschef.it>
2017-02-22 15:58:05 +0000

    plugin_expires.php edited online with Bitbucket

--- views/mail/plugin_expires.php
+++ views/mail/plugin_expires.php
@@ -16,15 +16,22 @@ include dirname(__FILE__).'/_header.php';
 				$user = get_user_by('ID', 1);
 				$name = $user->first_name . ' ' . $user->last_name;
 
-				$msg = sprintf(__("Hi %s,\n
-Salon Booking plugin is about to expire, are you satisfied by its features?\n
-Are you planning to buy a PRO license to keep on using it?\n
-If so <strong>click here to get a special 30 discount</strong> on your first purchase.\n
-If you are not willing to buy a PRO version help us on improving our plugin and answer to some questions.\n
-We'll be very happy to hear from your feedback.\n
-In case of doubts don't hesitate sending us an email to <strong>support@wpchef.it</strong>\n
-Have a nice day!\n
-The Salon Booking staff", 'salon-booking-system'), $name);
+				$msg = sprintf(__('Hi %s,<br /><br />
+
+<strong>Salon Booking</strong> plugin is about to expire, are you satisfied by its features?<br /><br />
+
+If so, are you planning to <strong><a href="http://salon.wordpresschef.it/salon-booking-plugin-pricing/">buy a PRO license</a></strong> to keep on using it?<br /><br /><br />
+
+If so <strong><a href="http://salon.wordpresschef.it/salon-booking-plugin-pricing/">click here</a></strong> to get a special <strong>30% discount</strong> on your first purchase.<br /><br />
+
+
+In case of any doubts don\'t hesitate sending us an email to <strong>support@wpchef.it</strong><br /><br />
+
+Have a nice day!<br /><br />
+
+The Salon Booking staff
+salon.wordpresschef.it
+', 'salon-booking-system'), $name);
 				$msg = nl2br($msg);
 				echo $msg;
 				?>
