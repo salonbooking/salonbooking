salonbooking/salonbooking

commit 459d27ba08b34e886dbeb5af65fa88f2f6dca7dc

Reset errors if suggested date/time are changed

src/SLN/Action/Ajax/CheckDate.php
@@ -32,6 +32,11 @@ class SLN_Action_Ajax_CheckDate extends SLN_Action_Ajax_Abstract
         }
         $ret['intervals'] = $this->getIntervalsArray();
 
+        if ($ret['intervals']['suggestedDate'] !== $this->date || $ret['intervals']['suggestedTime'] !== $this->time) {
+            unset($ret['errors']);
+            $ret['success'] = 1;
+        }
+
         return $ret;
     }
 

commit b45d3f5c2d9c80b4a8edaa42d89b23751a8a7280

#SBP-443 wrong success message, caused by commit 459d27ba

src/SLN/Action/Ajax/CheckDate.php
@@ -31,10 +31,12 @@ class SLN_Action_Ajax_CheckDate extends SLN_Action_Ajax_Abstract
             $ret = array('success' => 1);
         }
         $ret['intervals'] = $this->getIntervalsArray();
-
+        $isFromAdmin = isset($_POST['_sln_booking_date']);
+        if(!$isFromAdmin){
         if ($ret['intervals']['suggestedDate'] !== $this->date || $ret['intervals']['suggestedTime'] !== $this->time) {
             unset($ret['errors']);
             $ret['success'] = 1;
+        }
         }
 
         return $ret;