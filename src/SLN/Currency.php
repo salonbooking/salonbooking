7bc4c5b1286124ef81ee1e9e14b05746223bf352
Dimitri Grassi <contact@wordpresschef.it>
2017-05-19 07:51:44 +0000

    Currency.php edited online with Bitbucket

--- src/SLN/Currency.php
+++ src/SLN/Currency.php
@@ -6,7 +6,7 @@ class SLN_Currency
         'AUD' => array('name' => 'Australian Dollar', 'symbol' => 'A$', 'ASCII' => 'A&#36;'),
         'ARS' => array('name' => 'Argentine Peso', 'symbol' => '$', 'ASCII' => 'A&#36;'),
         'UAH' => array('name' => 'Ukrainian Hryvnia', 'symbol' => '₴', 'ASCII' => '&#8372;'),
-        'BRL' => array('name' => 'Brazilian Real', 'symbol' => 'R$', 'ASCII' => 'B&#36;'),
+        'BRL' => array('name' => 'Brazilian Real', 'symbol' => 'R$', 'ASCII' => '&#x0024;'),
         'BAN' => array('name' => 'Bosnia and Herzegovina
 convertible mark', 'symbol' => 'KM', 'ASCII' => ''),
         'CAD' => array('name' => 'Canadian Dollar', 'symbol' => '$', 'ASCII' => '&#36;'),
