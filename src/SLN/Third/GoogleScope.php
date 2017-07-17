9cd1339f214a908d7e2d8437683a52deb62d49a0
q <crapty94@gmail.com>
2017-07-17 14:05:16 +0400

    fixed booking url inside the event description on Google Calendar

--- src/SLN/Third/GoogleScope.php
+++ src/SLN/Third/GoogleScope.php
@@ -739,7 +739,7 @@ class SLN_GoogleCalendarEventFactory extends Google_Service_Calendar_Event {
         $notes = $booking->getNote();
         $desc .= "\n\n" . __('Booking notes', 'salon-booking-system') . ":\n" . (empty($notes) ? __("None", 'salon-booking-system') : $notes);
         $desc .= "\n\n" . __('Booking status', 'salon-booking-system') . ": " . SLN_Enum_BookingStatus::getLabel($booking->getStatus());
-        $desc .= "\n\n" . __('Booking URL', 'salon-booking-system') . ": " . get_edit_post_link($booking->getId());
+        $desc .= "\n\n" . __('Booking URL', 'salon-booking-system') . ": " . get_edit_post_link($booking->getId(), null);
 
         $title = $booking->getDisplayName() . " - " . $plugin->format()->datetime($booking->getStartsAt());
         sln_my_wp_log($title);
