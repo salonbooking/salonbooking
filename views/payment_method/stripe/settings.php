e96c330135e8cfedb15e80d767738ce3e3a7eb32
Marino Di Clemente <kernelfolla@gmail.com>
2017-10-16 11:06:32 +0200

    changed labels for stripe

--- views/payment_method/stripe/settings.php
+++ views/payment_method/stripe/settings.php
@@ -1,11 +1,11 @@
 
     <div class="col-xs-12"><h2 class="sln-box-title"><?php _e('Stripe account informations','salon-booking-system');?></h2></div>
         <div class="col-xs-12 col-sm-4 sln-input--simple">
-            <?php $adminSettings->row_input_text('pay_stripe_apiKey', __('Stripe API key', 'salon-booking-system')); ?>
+            <?php $adminSettings->row_input_text('pay_stripe_apiKey', __('Secret key', 'salon-booking-system')); ?>
             <p class="sln-input-help"><?php _e('-','Enter your Stripe api key');?></p>
         </div>
         <div class="col-xs-12 col-sm-4 sln-input--simple">
-            <?php $adminSettings->row_input_text('pay_stripe_apiKeyPublic', __('Stripe public API key', 'salon-booking-system')); ?>
+            <?php $adminSettings->row_input_text('pay_stripe_apiKeyPublic', __('Publishable key', 'salon-booking-system')); ?>
         <p class="sln-input-help"><?php _e('Enter your Stripe publishable api key','salon-booking-system');?></p>
         </div>
         <div class="col-xs-12 col-sm-4 sln-box-maininfo  align-top">
