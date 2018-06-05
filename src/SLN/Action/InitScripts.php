343bbd04b5310c7f1441a32edda35d2a0f68fb4d
Luca Castellone <luca.castellone@gmail.com>
2018-06-05 12:00:11 +0200

    Fix DatePicker and Wpml

--- src/SLN/Action/InitScripts.php
+++ src/SLN/Action/InitScripts.php
@@ -169,12 +169,7 @@ class SLN_Action_InitScripts
 
     public static function enqueueDateTimePicker()
     {
-        $date_lang = defined('ICL_LANGUAGE_CODE')
-            ? ICL_LANGUAGE_CODE
-            : (function_exists('pll_current_language')
-                ? pll_current_language()
-                : get_locale()
-        );
+        $date_lang =  get_locale();
         
         wp_enqueue_script(
             'smalot-datepicker',
